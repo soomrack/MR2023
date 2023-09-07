9bf6c282d5395140c37bd87a3a27ecb72bb61cfb
denispelm <144234708+denispelm@users.noreply.github.com>
2023-09-07 18:02:08 +0300

    Alice & Bob Question
    Optimized programm

--- pelmenev.denis.prog/dptask1.c
+++ pelmenev.denis.prog/dptask1.c
@@ -8,46 +8,41 @@ int t = 20;
 double infl = 0.07;
 double st = 0.09;
 int mw = 15000;
+int rp = 30000;
 
-double Bob (double Ss) {
-    int Xb = Ss - X0;
+double Perspective (char word) {
+    double pp = p;
+    double X = 0;
     double ppm;
-    ppm = Xb*(st/12)/(1-powf((1+st/12), (1-t*12)));
+    if (word == 'A') {
+        ppm = rp;
+        X = X0;
+    }
+    else if (word == 'B') {
+        ppm = (S-X0)*(st/12)/(1-powf((1+st/12), (1-t*12)));
+    }
+    else {
+        printf("Who is this? \n");
+        return 0;
+    };
     int i = 0;
-    double zp = 0;
-    double pp = p;
     while (i<t*12) {
         i+=1;
-        zp = (zp + pp - ppm - mw) * (1 + st/12);
+        X = (X + pp - ppm - mw) * (1 + st/12);
         if ((i%12==0)&&(i>11)) {
             pp = pp*(1+infl);
         };
     };
-    return zp;
-}
-
-double Alice (double Ss) {
-    double Sa;
-    Sa = Ss*powf((1+infl), t);
-    double Xa = X0;
-    int i = 0;
-    double pp = p;
-    int rp = 30000;
-    while (i<t*12) {
-        i+=1;
-        Xa = (Xa + pp - rp - mw) * (1 + st/12);
-        if ((i%12==0)&&(i>11)) {
-            pp = pp*(1+infl);
-        };
-    };
-    Xa = Xa - Sa;
-    return Xa;
+    return X;
 }
 
 int main(void) {
     double a, b;
-    a = Alice(S);
-    b = Bob(S);
+    char c, d;
+    c = 'A';
+    d = 'B';
+    a = Perspective(c);
+    b = Perspective(d);
     printf("%2f\n", a, "\n");
     printf("%2f\n", b, "\n");
     return 0;
