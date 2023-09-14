01eb9db77f2dc07963779023079dd40fca221af8
rybyakov19 <144235855+rybyakov19@users.noreply.github.com>
2023-09-14 19:29:23 +0300

    Add files via upload

--- Rybyakov.Daniil/Alice and bob.c
+++ Rybyakov.Daniil/Alice and bob.c
@@ -1,29 +1,62 @@
 #include <stdio.h>
 #include <math.h>
 
-double main()
+
+//переменные
+typedef long long int Money;  //копейки
+
+int MONTH = 240;  // мес€цы
+double CREDITPERCENT = 0.07;  // процент по кредиту
+double m_koef = 0.07 / 12;  // коэфф расчета платежа
+double DEPOSIT = 0.09;  //проццент в банке
+
+Money CREDIT = 19 * 1000 * 1000 * 100;  // сумма кредита
+Money CREDITPAYMENT;  //платеж по кредиту
+Money FLAT = 20 * 1000 * 1000 * 100;  //стоимость квартиры
+
+
+struct Person 
 {
-	//переменные
-	int salary = 200000, time = 240, i;
-	int rent = 30000, credit = 19000000;
-	double abank = 1000000.0, bbank = 0.0, deposit = 0.09, abalance, bbalance;
-	double creditpercent = 0.07, creditpayment, m = 0.07/12;
+	Money balance;
+	Money bank;
+	Money total;
+	Money salary;
+	Money rent;
+};
 
-	abalance = salary - rent;
-	creditpayment = credit*((m * pow(1 + m, time)) / (pow(1 + m, time) - 1));
-	bbalance = salary - creditpayment;
-	bbank = bbalance;
+struct Person Alice = { 0.0,  1 * 1000 * 1000 * 100,  0.0,  200 * 1000 * 100,  30 * 1000 * 100};
+struct Person Bob = {0.0,  0.0,  0.0,  200 * 1000 * 100,  0.0};
 
-	//расчет
-	for (i = 1; i <= time; ++i){
-		abank += (abank * (deposit / 12));
-		bbank += (bbank * (deposit / 12));
-		abank -= abank * (0.07 / 12);
-		bbank -= bbank * (0.07 / 12);
-		abank += abalance;
-		bbank += bbalance;
-	}
 
-	printf("%f   %f", abank, bbank);
+Money creditpay(int credit, double m, int time) 
+{
+	int result;
+	result = credit * ((m * pow(1 + m, time)) / (pow(1 + m, time) - 1));  //платеж по кредиту
+	return result;
+}
 
+
+main()
+{
+	CREDITPAYMENT = creditpay(CREDIT, m_koef, MONTH);
+
+	Alice.balance = Alice.salary - Alice.rent;  // ежемес€чный остаток денег
+	Bob.balance = Bob.salary - CREDITPAYMENT;  //ежемес€чный остаток денег
+	Bob.bank = Bob.balance;
+
+		for (int i = 1; i <= MONTH; ++i)
+		{
+			Alice.bank += (Alice.bank * (DEPOSIT / 12));
+			Bob.bank += (Bob.bank * (DEPOSIT / 12));
+			Alice.bank -= Alice.bank * (0.07 / 12);
+			Bob.bank -= Bob.bank * (0.07 / 12);
+			Alice.bank += Alice.balance;
+			Bob.bank += Bob.balance;
+			FLAT += FLAT * (0.01 / 12);
+		}
+
+	Alice.total = Alice.bank / 100;
+	Bob.total = (Bob.bank + FLAT) / 100;
+
+	printf("%d   %d", Alice.total, Bob.total);
 }
