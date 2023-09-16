4fb20c58920ccef87d7d96bcb51451c64b9cabe9
denispelm <144234708+denispelm@users.noreply.github.com>
2023-09-16 14:39:03 +0300

    Alice & Bob Question Update

--- pelmenev.denis.prog/dptask1.c
+++ pelmenev.denis.prog/dptask1.c
@@ -1,58 +1,93 @@
 #include <stdio.h>
 #include <math.h>
 
-int S = 20000000;
-int p = 200000;
-int X0 = 1000000;
-int t = 20;
-double infl = 0.07;
-double st = 0.09;
-int mw = 20000;
-int rp = 30000;
 
-double Perspective (char word) {
-    double pp = p;
-    double X = 0;
-    double ppm;
-    double Xa = 0;
-    double mww = mw;
-    if (word == 'A') {
-        ppm = rp;
-        X = X0;
-        Xa = S*powf((1+infl), t);
-    }
-    else if (word == 'B') {
-        ppm = (S-X0)*(st/12)/(1-powf((1+st/12), (1-t*12)));
-    }
-    else {
+typedef long long int Money;  // рубли
+
+
+struct Person
+{
+    Money capital;
+    Money income;              // заработок
+    Money life_spendings;      // траты на существование
+    Money monthly_payment;     // траты на квартиру (проживание или погашение ипотеки)
+    Money overpayment;         // переплата при покупке квартиры
+} Alice, Bob, Intermediary;    // создание структур для Элис и Боба, а также структуры-посредника
+
+
+const Money PRICE = 20 * 1000 * 1000;     // стоимость квартиры в рублях
+const Money SALARY = 200 * 1000;          // зарплата каждого в рублях
+const Money START_CAPITAL = 1000 * 1000;  // стартовый капитал каждого в рублях
+const int YEARS = 20;
+const double INFLATION = 0.07;            // процент инфляции в год
+const double BANK_RATE = 0.09;            // годовая ставка в банке
+const Money LIFE_SPENDINGS = 25 * 1000;   // траты на жизнь в рублях
+
+
+void preparation(char name)
+{
+    if (name == 'A') {
+        Intermediary.monthly_payment = 40 * 1000;
+        Intermediary.capital = START_CAPITAL;
+        Intermediary.overpayment = PRICE * powf((1 + INFLATION), YEARS);
+    } else if (name == 'B') {
+        Intermediary.monthly_payment = (PRICE - START_CAPITAL) * (BANK_RATE / 12) / (1 - powf((1 + BANK_RATE / 12), (1 - YEARS * 12)));
+        Intermediary.capital = 0;
+        Intermediary.overpayment = Intermediary.monthly_payment * 12 * YEARS;
+    } else {
         printf("Who is this? \n");
-        return 0;
+        return;
     };
-    int i = 1;
-    while (i<t*12) {
-        i+=1;
-        X = (X + pp - ppm - mww) * (1 + st/12);
-        if ((i%2==0)&&(i>1)) {
-            mww = mww*(1+infl/6);
-            if ((i%12==0)&&(i>11)) {
-                pp = pp*(1+infl/2);
-                if (word == 'A') {
-                    ppm = ppm*(1+infl);
-                };
+    return;
+}
+
+
+void counting (int years)
+{
+    for (int k = 1; k <= years * 12; k++) {
+        Intermediary.capital = (Intermediary.capital + Intermediary.income - Intermediary.monthly_payment 
+        - Intermediary.life_spendings) * (1 + BANK_RATE / 12);
+        if ((k % 3 == 0) && (k > 2)) {
+            Intermediary.life_spendings = Intermediary.life_spendings * (1 + INFLATION / 4);
+            if ((k % 12 == 0) && (k > 11)) {
+                Intermediary.income = Intermediary.income * (1 + INFLATION / 2);
             };
         };
     };
-    X = X - Xa;
-    return X;
 }
 
-int main(void) {
-    double a, b, c;
-    a = Perspective(65);
-    b = Perspective(66);
-    c = Perspective(67);
-    printf("%2f\n", a, "\n");
-    printf("%2f\n", b, "\n");
-    printf("%2f\n", c, "\n");
+
+void estimate (char name) 
+{
+    preparation(name);
+
+    Intermediary.income = SALARY;
+    Intermediary.life_spendings = LIFE_SPENDINGS;
+    
+    counting(YEARS);
+
+    if (name == 'A') {
+        Intermediary.capital = Intermediary.capital - Intermediary.overpayment;
+    };
+    Intermediary.overpayment = Intermediary.overpayment - PRICE;
+    if (name == 'A') {
+        Alice = Intermediary;
+    } else {
+        Bob = Intermediary;
+    };
+    return;
+}
+
+
+int main(void) 
+{
+    estimate(65);
+    estimate(66);
+    printf("A. Capital: %d\t", Alice.capital); printf("A. Income: %d\t", Alice.income); printf("A. Life spendings: "
+    "%d\t", Alice.life_spendings); printf("A. Monthly payment: %d\t", Alice.monthly_payment); printf("A. Overpayment: " 
+    "%d\n", Alice.overpayment);
+    printf("B. Capital: %d\t", Bob.capital); printf("B. Income: %d\t", Bob.income); printf("B. Life spendings: "
+    "%d\t", Bob.life_spendings); printf("B. Monthly payment: %d\t", Bob.monthly_payment); printf("B. Overpayment: " 
+    "%d\n", Bob.overpayment);
     return 0;
 }
