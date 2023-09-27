c396264577e012f5c806e93cd023cdb0528e738b
14Og <kiznick3500@gmail.com>
2023-09-27 14:44:14 +0300

    Added elements of incapsulation

--- Kiz.Nikolai/task1/inc/capital.h
+++ Kiz.Nikolai/task1/inc/capital.h
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <string.h>
 
-
+typedef long int money;  // ₽ 
 
 typedef struct {
     char * name;
@@ -10,16 +10,21 @@ typedef struct {
     variables that describes: person's payback, person's expenses for apartment (rent or mortgage), 
     person's other expenses (groceries, utility bills) and bank debt (for Bob only, for alice = 0)
     */
-    long int monthlyIncome, apartmentExpenses, otherExpenses, bankDebt;   
+    money  monthlyIncome, apartmentExpenses, otherExpenses, bankDebt;   
     const __uint8_t inflation, interestRate, mortgageInterest;
-    long int currentCapital;  //  currentCapital will increase from month to month, thats a main variable in this task
+    money currentCapital;  //  currentCapital will increase from month to month, thats a main variable in this task
 } Capital;  // struct Capital, defines a person (Alice or Bob)
 
 
 
-void monthOperation(Capital * this, const int monthNum);  // monthly routine, calculate capital changes every month 
-double calcBobApartmentFee(long debt, const __uint8_t percent);  //  function calculate Bob's mortgage monthly fee
-long int processFunction(Capital * this, const int totalMonthNum); // main process function with lopp
+void inflationProcessor(Capital * this, const int monthNum);
+void expensesProcessor(Capital * this, const int monthNum);
+void bankDepositIncome(Capital * this);
+void paybackProcessor(Capital * this, const int yearNum, const int monthNum);
+void monthOperation(Capital * this, const int yearNum, const int monthNum);  // monthly routine, calculate capital changes every month 
+double calcBobApartmentFee(money debt, const __uint8_t percent);  //  function calculate Bob's mortgage monthly fee
+money processFunction(Capital * this, const int totalYearNum); // main process function with lopp
+void mainFunction(Capital * alice, Capital * bob, const int totalYearNum);
 
 
 
--- Kiz.Nikolai/task1/src/main.c
+++ Kiz.Nikolai/task1/src/main.c
@@ -9,14 +9,14 @@ int main(int argc, char * argv[]) // run this function with args : total amount 
         printf("Not enough arguments passed into main.\n");
         return 1;  //  pass args when execute!!!
     } 
-    int totalMonth = atoi(argv[1]);
-    long otherExpenses = (long)atoi(argv[2]);
+    const  int totalYears = atoi(argv[1]);
+    const money otherExpenses = (long)atoi(argv[2]);
     Capital bob = {
         .name = "Bob",
         .monthlyIncome = 200 * 1000,  // rubles
         .otherExpenses = otherExpenses,
         .bankDebt = 19 * 1000 * 1000,
-        .inflation = 3,
+        .inflation = 5,
         .interestRate = 9,
         .mortgageInterest = 7,
         .currentCapital = 0,
@@ -38,14 +38,6 @@ int main(int argc, char * argv[]) // run this function with args : total amount 
     };  //  defined Alice
     
 
-    long int aliceTotalCapital = processFunction(&alice, totalMonth);  //  calculate Alice total capital after 20 years
-    long int bobTotalCapital = processFunction(&bob, totalMonth) 
-    + (20 * 1000 * 1000 * pow (1 + bob.inflation/100, 20));  //  calculate Bob capital
-
-
-    printf("ALICE TOTAL CAPITAL: %lu rubles.\n", aliceTotalCapital);
-    printf("BOB TOTAL CAPITAL: %lu rubles.\n", bobTotalCapital);
-    printf("Finally, %s earned more by %i rubles.\n", aliceTotalCapital > bobTotalCapital ? "Alice" : "Bob",
-                                                             abs(aliceTotalCapital - bobTotalCapital));
+    mainFunction(&alice, &bob, totalYears);
     return 0;
 }
