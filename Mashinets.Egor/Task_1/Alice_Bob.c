7ff567085d871af92fa039684f074236b3a8e328
Apchexba <egormash03@gmail.com>
2023-10-18 15:25:33 +0300

    13

--- Mashinets.Egor/Task_1/Alice_Bob.c
+++ Mashinets.Egor/Task_1/Alice_Bob.c
@@ -7,7 +7,7 @@ typedef long long int kopeyki;
 kopeyki amortization = 5000 * 100;//monthly contribution for "airbag"(treatment, technique breakdown)
 kopeyki flat_price = 10 * 1000 * 1000 * 100;
 double inflation = 0.07;
-
+int count;
 struct Person {
     kopeyki capital;
     kopeyki salary;
@@ -18,39 +18,53 @@ struct Person {
 
 struct Person Alice, Bob;
 
-void alice_init() {
+
+void alice_init()
+ {
     Alice.capital = 1000 * 1000 * 100;
     Alice.salary = 200 * 1000 * 100;
     Alice.expenses = 50 * 1000 * 100;
     Alice.rent_price = 40 * 1000 * 100;
 }
 
-void bob_init() {
+
+void bob_init()
+ {
     Bob.capital = 0 * 100;
     Bob.salary = 200 * 1000 * 100;
     Bob.expenses = 50 * 1000 * 100;
     Bob.mortgage_price = 100 * 1000 * 100;
 }
 
-void alice_salary_income() {
+
+void alice_salary_income(const int mounth) {
+if (mounth == 12) {
+    Alice.capital += Alice.salary;
+}
     Alice.capital += Alice.salary;
     Alice.salary *= (inflation / 12 + 1);
+
 }
 
-void bob_salary_income() {
+
+void bob_salary_income()
+ {
     Bob.capital += Bob.salary;
     Bob.salary *= (inflation/12 + 1);
 }
- 
-void alice_deposite_income() {
+
+
+void alice_deposite_income()
+ {
    int bank_p;
     if (Alice.capital < 1000 * 1000 * 10 * 100) {
         bank_p = 11;
     } else {
         bank_p = 9;
     }
-    Alice.capital += (kopeyki)(Alice.capital * (bank_p/100.0/12.0));
+    Alice.capital += (kopeyki)(Alice.capital * (bank_p / 100.0 / 12.0));
 }
+
 
 void bob_deposite_income() {
     int bank_p;
@@ -62,40 +76,58 @@ void bob_deposite_income() {
     Bob.capital += (kopeyki)(Bob.capital*(bank_p/100.0/12.0));
 }
 
+
 void alice_expenses() {
     Alice.capital -= Alice.expenses;
     Alice.expenses *= (inflation / 12 + 1);
 }
 
-void bob_expenses() {
+
+void bob_expenses()
+{
     Bob.capital -= Bob.expenses;
     Bob.expenses *= (inflation / 12 + 1);
 }
 
-void alice_rent() {
+
+void alice_rent() 
+{
     Alice.capital -= Alice.rent_price;
     Alice.rent_price *= (inflation / 12 + 1);
 }
 
-void bob_rent() {
+
+void bob_rent() 
+{
+
     Bob.capital -= Bob.mortgage_price;
     Bob.mortgage_price *= (inflation / 12 + 1);
+
 }
-void alice_extra_spend() {
+
+
+void alice_extra_spend() 
+{
+
     Alice.capital -= amortization;  
+
 }
 
 
-void bob_extra_spend() {
+void bob_extra_spend() 
+{
     
     Bob.capital -= amortization; 
+
 }
 
 
-void simulation() {
+void simulation() 
+{
+
     int mounth = 9, year = 2023;
  while(year !=2053 && mounth !=8) {
-    alice_salary_income();
+    alice_salary_income(mounth);
     alice_deposite_income();
     alice_rent();
     alice_expenses();
@@ -111,19 +143,21 @@ void simulation() {
     if (mounth == 13) {
         mounth = 1;
         ++year;
-        Alice.capital = Alice.salary;
     }
  }
 }
 
 
-void print_winner() {
+void print_winner() 
+{
+
     kopeyki Bob_capital = (Bob.capital + flat_price) / 100;
     kopeyki Alice_capital = (Alice.capital) / 100;
     printf("|%0.2lld | %0.2lld|\n %s", Alice_capital, Bob_capital,  (Alice_capital > Bob_capital) ? "Alice will accumulate more capital":"Bob will accumulate more capital");
 }
 
-int main() {
+int main() 
+{
 
 //setlocale(LC_ALL, "Ru");
 
