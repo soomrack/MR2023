df512a31be8a45c6beb9df7efb3ed75aa4818789
rybyakov19 <144235855+rybyakov19@users.noreply.github.com>
2023-10-18 14:09:29 +0300

    123

--- Rybyakov.Daniil/alice and bob.c
+++ Rybyakov.Daniil/alice and bob.c
@@ -2,169 +2,214 @@
 #include <math.h>
 
 
-//переменные
-typedef long long int Money;  //копейки
+typedef long long int Money;  // копейки
 
-int MONTH = 240;  // месяцы
-double CREDITPERCENT = 0.07;  // процент по кредиту
-double m_koef = 0.07 / 12;  // коэфф расчета платежа
+int YEAR = 2023;
+int MONTH = 9;
+int MORTAGE_PERIOD = 240;  
 
-Money CREDIT = 19 * 1000 * 1000 * 100;  // сумма кредита
-Money CREDITPAYMENT;  //платеж по кредиту
-Money FLAT = 20 * 1000 * 1000 * 100;  //стоимость квартиры
+double FLAT_TAX = 0.05;
+double M_KOEF = 0.07 / 12;  // коэфф расчета платежа
+
+Money MORTAGE = 19 * 1000 * 1000 * 100;  // сумма кредита
+Money MORTAGE_PAYMENT;  
+Money FLAT = 20 * 1000 * 1000 * 100;  
 
 
-struct Person
-{
-	Money balance;  //кол-во денег после расходов на жизнь
-	Money bank;  //счте в банке
-	Money total;  //конечный результат
-	Money salary;  
-	Money rent;  //стоимость аренды
-	double deposit;  //процент счета в банке
+struct Person {
+    Money balance;  // кол-во денег после расходов на жизнь
+    Money bank;  
+    Money capital;  // конечный результат
+    Money salary;  
+    Money rent;  
+    Money spending;  // траты на жизнь
+    Money deposit_bonus;
+    double deposit;  // процент счета в банке
 };
+
 
 struct Person Alice;
 struct Person Bob;
 
+
 void initialization_Alice()
 {
-	Alice.balance = 0;
-	Alice.bank = 1 * 1000 * 1000 * 100;
-	Alice.total = 0;
-	Alice.salary = 200 * 1000 * 100;
-	Alice.rent = 30 * 1000 * 100;
-	Alice.deposit = 0.09;
+    Alice.bank = 1 * 1000 * 1000 * 100;
+    Alice.capital = 0;
+    Alice.salary = 200 * 1000 * 100;
+    Alice.rent = 30 * 1000 * 100;
+    Alice.deposit = 0.09;
+    Alice.spending = 15 * 1000 * 100;
+    Alice.deposit_bonus = 5 * 1000 * 1000 * 100;
 }
 
 
 void initialization_Bob()
 {
-	Bob.balance = 0;
-	Bob.bank = 0;
-	Bob.total = 0;
-	Bob.salary = 200 * 1000 * 100;
-	Bob.rent = 0;
-	Bob.deposit = 0.09;
+    Bob.bank = 0;
+    Bob.capital = 0;
+    Bob.salary = 200 * 1000 * 100;
+    Bob.rent = 0;
+    Bob.deposit = 0.09;
+    Bob.spending = 15 * 1000 * 100;
+    Bob.deposit_bonus = 5 * 1000 * 1000 * 100;
 }
 
 
-void creditpay()  //расчет платежа по кредиту
+void mortage_pay(Money MORTAGE, double M_KOEF, int MORTAGE_PERIOD)  // расчет платежа по кредиту
 {
-	CREDITPAYMENT = CREDIT * ((m_koef * pow(1 + m_koef, MONTH)) / (pow(1 + m_koef, MONTH) - 1));  //платеж по кредиту
+    MORTAGE_PAYMENT = MORTAGE * ((M_KOEF * pow(1 + M_KOEF, MORTAGE_PERIOD)) / (pow(1 + M_KOEF, MORTAGE_PERIOD) - 1));
 }
 
 
-void Alice_free_mooney()
+void time_count()  // счетчик лет
 {
-	Alice.balance = Alice.salary - Alice.rent;  // ежемесячный остаток денег
+    MONTH += 1;
+    if (MONTH == 13) {
+        MONTH = 1;
+    }
+
+    if (MONTH % 12 == 0) {
+        YEAR += 1;
+    }
 }
 
 
-void Bob_free_mooney()
+void Alice_salary(int condition_year, int condition_month)
 {
-	Bob.balance = Bob.salary - CREDITPAYMENT;  //ежемесячный остаток денег
+    if ((YEAR == condition_year) && (MONTH == condition_month)) {  // Алиса теряет зарплату
+        Alice.salary = 0;
+    }
+    else {
+        Alice.salary = 200 * 1000 * 100;
+    }
+    Alice.bank += Alice.salary;
 }
 
 
-void count_Alice(int now_year, int now_month)  //расчет баланса алисы
+void Bob_salary(int condition_year, int condition_month)
 {
-	int year = 0;
-	int i;
-	Money deposit_bonus = 5 * 1000 * 1000 * 100;  //условие увеличения ставки в банке
-	for (i = 0; i <= MONTH; ++i)
-	{
-		if (i % 12 == 0)  //вычисление года
-		{
-			year += 1;
-		}
-
-		if ((year == now_year) && (i % 12 == now_month))  //Алиса теряет зарплату
-		{
-			Alice.balance = 0;
-		}
-
-		if (Alice.bank / deposit_bonus == 1)  //увеличение ставки
-		{
-			deposit_bonus += 5 * 1000 * 1000 * 100;
-			Alice.deposit += 0.01;
-		}
-
-		if ((year % 10 == 0) && (i % 12 == 0))  //траты на зубного
-		{
-			Alice.bank -= 500 * 1000 * 100;
-		}
-
-		Alice.bank += Alice.bank * (Alice.deposit / 12);
-		Alice.bank -= Alice.bank * (0.07 / 12);
-		Alice.bank += Alice.balance;
-	}
+    if ((YEAR == condition_year) && (MONTH == condition_month)) {  // Боб теряет зарплату
+        Bob.salary = 0;
+    }
+    else {
+        Bob.salary = 200 * 1000 * 100;
+    }
+    Bob.bank += Bob.salary;
 }
 
 
-void count_Bob(int now_year, int now_month)  //расчет баланса боба
+void deposit_bonus() 
 {
-	int year = 0;
-	int i;
-	Money deposit_bonus = 5 * 1000 * 1000 * 100;  //условие увеличения ставки в банке
-	for (i = 0; i <= MONTH; ++i)
-	{
-		if (i % 12 == 0)  //вычисление года
-		{
-			year += 1;
-		}
+    if (Alice.bank / Alice.deposit_bonus == 1) {  // увеличение ставки
+        Alice.deposit_bonus += 5 * 1000 * 1000 * 100;
+        Alice.deposit += 0.005;
+    }
 
-		if (Bob.bank / deposit_bonus == 1)  //увеличения ставки в банке
-		{
-			deposit_bonus += 5 * 1000 * 1000 * 100;
-			Bob.deposit += 0.01;
-		}
-
-		Bob.bank += Bob.bank * (Bob.deposit / 12);
-		Bob.bank -= Bob.bank * (0.07 / 12);
-		Bob.bank += Bob.balance;
-	}
+    if (Bob.bank / Bob.deposit_bonus == 1) {
+        Bob.deposit_bonus += 5 * 1000 * 1000 * 100;
+        Bob.deposit += 0.005;
+    }
 }
 
 
-void count_Flat()  //расчет стоимости квартиры
+void Alice_dentist()
 {
-	for (int i = 0; i <= MONTH; ++i)
-	{
-		FLAT += FLAT * (0.01 / 12);
-	}
+    if (((YEAR-2023) % 10 == 0) && (MONTH == 3)) {  // траты на зубного
+        Alice.bank -= 500 * 1000 * 100;
+    }
 }
 
 
-void final_Alice()  //финальный баланс алисы в рублях
+void flat_tax()  // налог на квартиру
 {
-	Alice.total = Alice.bank / 100;
+    if (MONTH == 9) {
+        Bob.bank -= FLAT * FLAT_TAX;
+    }
 }
 
 
-void final_Bob()  //финальный баланс боба в рублях
+void Alice_spend_money()  
 {
-	Bob.total = (Bob.bank + FLAT) / 100;
+    if (MONTH == 1) {
+        Alice.spending += Alice.spending * 0.03;
+    }
+    Alice.bank -= Alice.spending - Alice.rent;
 }
 
 
-void result()  //вывод результата
+void Bob_spend_money()
 {
-	printf("%lld   %lld", Alice.total, Bob.total);
+    if (MONTH == 1) {
+        Bob.spending += Bob.spending * 0.03;
+    }
+    Bob.bank -= Bob.spending - MORTAGE_PAYMENT;
+}
+
+
+void Alice_bank()
+{
+    Alice.bank += Alice.bank * (Alice.deposit / 12);
+    Alice.bank -= Alice.bank * (0.07 / 12);
+}
+
+
+void Bob_bank()
+{
+    Bob.bank += Bob.bank * (Bob.deposit / 12);
+    Bob.bank -= Bob.bank * (0.07 / 12);
+    FLAT += FLAT * (0.01 / 12);
+}
+
+
+void total_counting(int condition_year, int condition_month)
+{
+    for (int month = 0; month <= MORTAGE_PERIOD; ++month) {
+        time_count();
+        deposit_bonus();
+
+        Alice_salary(condition_year, condition_month);
+        Alice_dentist();
+        Alice_spend_money();
+        Alice_bank();
+
+        Bob_salary(condition_year, condition_month);
+        flat_tax();
+        Bob_spend_money();
+        Bob_bank();
+    }
+}
+
+
+void final_Alice()  
+{
+    Alice.capital = Alice.bank / 100;
+}
+
+
+void final_Bob()  
+{
+    Bob.capital = (Bob.bank + FLAT) / 100;
+}
+
+
+void result() 
+{
+    printf("%lld   %lld", Alice.capital, Bob.capital);
 }
 
 
 main()
 {
-	creditpay();
-	initialization_Alice();
-	initialization_Bob();
-	Alice_free_mooney();
-	Bob_free_mooney();
-	count_Flat();
-	count_Alice(15, 8);
-	count_Bob(1, 3);
-	final_Alice();
-	final_Bob();
-	result();
+    mortage_pay(MORTAGE, M_KOEF, MORTAGE_PERIOD);
+
+    initialization_Alice();
+    initialization_Bob();
+
+    total_counting(2040, 3);
+
+    final_Alice();
+    final_Bob();
+
+    result();
 }
