5fd1500d0aa3570abbdbb2d335e8acf72575666d
ZabegalovAP <144235685+ZabegalovAP@users.noreply.github.com>
2023-10-18 16:22:32 +0300

    Create AandB.c

---
+++ ZabegalovAnton/AandB.c
@@ -0,0 +1,159 @@
+#include <stdio.h>
+#include <math.h>
+
+typedef long long int Money; // копейка
+
+struct Person{
+    Money capital;
+    Money deposit;
+    Money salary;
+    Money food;
+    Money utilities; // комунальные услуги
+};
+
+Money APARTMENT = 20 * 1000 * 1000 * 100; // стоимость квартиры
+const Money PAY = 14730680; // платеж по ипотеке 
+const Money PAY_LAST = 14202190; // последний платеж по ипотеке
+const double PERCENT_1 = 0.09; // процент по вкладу
+const double PERCENT_2 = 0.11; // увеличенная ставка по вкладу
+const double INFLATION = 0.07; // инфляция
+const Money RENT = 30 * 1000 * 100;  // квартплата
+int n = 0;
+
+
+void profit_deposit(struct Person* human, double percent_1, double percent_2)
+{
+    double percent;
+        
+    if(human->deposit >= 5*1000*1000*100) //увеличение ставки по вкладу за отстаток 5 млн
+        percent = percent_2;
+    else 
+        percent = percent_1;
+
+    human->deposit = human->deposit + human->deposit * percent / 12.0;
+    human->deposit = human->deposit + human->salary;
+}
+
+
+void price_apartment(Money* apartment, double inflation)
+{
+    *apartment = *apartment + *apartment * inflation / 12.0;
+}
+
+
+void spend_food(struct Person* human)
+{
+    human->deposit = human->deposit - human->food;
+}
+
+
+void spend_utilities(struct Person* human)
+{
+    human->deposit = human->deposit - human->utilities;
+}
+
+
+void spend_rent(struct Person* human, Money rent)
+{
+    human->deposit = human->deposit - rent;
+}
+
+
+void spend_mortgage(struct Person* human, Money pay)
+{
+    human->deposit = human->deposit - pay;
+}
+
+
+void spend_dentist(struct Person* human, int *year, int *starting_point)
+{
+    if(*year - *starting_point == 10){
+        human->deposit = human->deposit - 500 * 1000 * 100;
+        *starting_point = *year;
+        n++;
+    }
+}
+
+
+void init_bob(struct Person *human)
+{
+    human->capital = 0;
+    human->deposit = 0;
+    human->salary = 200 * 1000 * 100;
+    human->food = 15 * 1000 * 100;
+    human->utilities = 5 * 1000 * 100;
+}
+
+
+void init_alice(struct Person *human)
+{
+    human->capital = 0;
+    human->deposit = 1 * 1000 * 1000 * 100;
+    human->salary = 200 * 1000 * 100;
+    human->food = 15 * 1000 * 100;
+    human->utilities = 5 * 1000 * 100;
+}
+
+
+void simulation(struct Person* bob, struct Person* alice)
+{
+    int month = 9;
+    int year = 2023;
+    int starting_point_alice = year; // начало отсчета для обращения к зубному 
+    int starting_point_bob = year;
+
+    while(!(month == 9 && year == 2043)){
+
+        if(month == 13){
+            month = 1;
+            year++;
+        }
+        
+        profit_deposit(bob, PERCENT_1, PERCENT_2);
+        profit_deposit(alice, PERCENT_1, PERCENT_2);
+        price_apartment(&APARTMENT, INFLATION);
+        spend_food(bob);
+        spend_food(alice);
+        spend_utilities(bob);
+        spend_utilities(alice);
+        spend_rent(alice, RENT);
+        spend_dentist(bob, &year, &starting_point_bob);
+        spend_dentist(alice, &year, &starting_point_alice);
+
+        if(month == 8 && year == 2043)
+            break;
+
+        spend_mortgage(bob, PAY);
+
+        month++;
+    }
+    
+    bob->deposit = bob->deposit - PAY_LAST; // последний платеж по ипотеке
+    bob->capital = bob->deposit + APARTMENT;
+    alice->capital = alice->deposit;
+}
+
+
+void printf_result(Money alice_capital, Money bob_capital)
+{
+    if(alice_capital > bob_capital)
+        printf("не бери кредит\n");
+    else
+        printf("бери кредит\n");
+
+    printf("Alice=%lld\nBob=%lld\n", alice_capital, bob_capital);
+}
+
+
+int main()
+{
+    struct Person alice, bob;
+
+    init_alice(&alice);
+    init_bob(&bob);
+    simulation(&bob, &alice);
+    printf_result(alice.capital, bob.capital);
+    printf("%d\n", n);
+
+    return 0;
+} 
