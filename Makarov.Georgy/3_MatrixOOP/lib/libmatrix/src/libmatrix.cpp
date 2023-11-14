239053ec02d660efeaf7785a1e3cf2b860fb742e
Georgy Makarov <makarov.prdx@yandex.ru>
2023-11-14 23:14:12 +0300

    Task 3 fix

--- Makarov.Georgy/3_MatrixOOP/lib/libmatrix/src/libmatrix.cpp
+++ Makarov.Georgy/3_MatrixOOP/lib/libmatrix/src/libmatrix.cpp
@@ -1,28 +1,5 @@
 #include "libmatrix.h"
 
-void Matrix::error(enum ErrorType error_type) {
-    switch (error_type) {
-        case MEMORY_ERROR:
-            std::cout << "Memory allocation error" << std::endl;
-            break;
-        case BAD_MATRIX_ERROR:
-            std::cout << "Incalculable or empty matrix error" << std::endl;
-            break;
-        case COLS_ROWS_ERROR:
-            std::cout << "Wrong number of columns or rows" << std::endl;
-            break;
-        case DET_ERROR:
-            std::cout << "Determinant can be only calculated for 1x1, 2x2 or 3x3 matrix" << std::endl;
-            break;
-        case EXP_LEVEl_ERROR:
-            std::cout << "Exponential level cannot be smaller than 1" << std::endl;
-            break;
-        default:
-            std::cout << "Undefined error" << std::endl;
-            break;
-    }
-}
-
 
 void Matrix::fill(enum MatrixType matrix_type) {
     switch (matrix_type) {
@@ -48,7 +25,7 @@ void Matrix::fill(enum MatrixType matrix_type) {
                         if (row == col)
                             data[row * cols + col] = 1.;
                     }
-            else error(COLS_ROWS_ERROR);
+            else throw MatrixException("Wrong number of columns or rows");
             break;
 
         case (UNFILLED):
@@ -57,30 +34,41 @@ void Matrix::fill(enum MatrixType matrix_type) {
 }
 
 
+Matrix::Matrix(const unsigned int n) {
+    if (n < SIZE_MAX / sizeof(matrix_item) / n) {
+        rows = n;
+        cols = n;
+        data = new matrix_item[n * n];
+        if (data == nullptr)
+            throw MatrixException("Memory allocation error");
+    } else throw MatrixException("Memory allocation error");
+}
+
+
 Matrix::Matrix(size_t rows_amount, size_t cols_amount, MatrixType matrix_type) {
+    if (rows_amount >= SIZE_MAX / sizeof(matrix_item) / cols_amount)
+        throw MatrixException("Memory allocation error");
     if (rows_amount != 0 || cols_amount != 0) {
         data = new matrix_item[rows_amount * cols_amount];
         if (data == nullptr)
-            error(MEMORY_ERROR);
-        else {
-            rows = rows_amount;
-            cols = cols_amount;
-            fill(matrix_type);
-        }
+            throw MatrixException("Memory allocation error");
+        rows = rows_amount;
+        cols = cols_amount;
+        fill(matrix_type);
     }
 }
 
 
 matrix_item Matrix::get(size_t row, size_t col) {
-    if (row > rows || col > cols) {
-        error(COLS_ROWS_ERROR);
-        return 0.;
-    } else return data[col + cols * row];
+    if (row > rows || col > cols)
+        throw MatrixException("Wrong number of columns or rows");
+    else return data[col + cols * row];
 }
 
 
 void Matrix::set(size_t row, size_t col, matrix_item item) {
-    if (row > rows || col > cols) error(COLS_ROWS_ERROR);
+    if (row > rows || col > cols)
+        throw MatrixException("Wrong number of columns or rows");
     else data[col + cols * row] = item;
 }
 
@@ -95,128 +83,162 @@ void Matrix::print() {
             std::cout << std::endl;
         }
         std::cout << std::endl;
-    } else {
-        error(BAD_MATRIX_ERROR);
-    }
+    } else throw MatrixException("Bad matrix error");
 }
 
 
 Matrix::Matrix(const Matrix &M) {
+    if (rows < SIZE_MAX / sizeof(matrix_item) / cols) {
+        rows = M.rows;
+        cols = M.cols;
+        data = new matrix_item[rows * cols];
+        if (data == nullptr)
+            throw MatrixException("Memory allocation error");
+        std::copy(M.data, M.data + rows * cols, data);
+    } else throw MatrixException("Memory allocation error");
+}
+
+
+Matrix::Matrix(Matrix &&M) noexcept {
     rows = M.rows;
     cols = M.cols;
-    data = new matrix_item[rows * cols];
-    std::copy(M.data, M.data + rows * cols, data);
+    data = M.data;
+    M.rows = 0;
+    M.cols = 0;
+    M.data = nullptr;
 }
 
 
 Matrix &Matrix::operator=(const Matrix &M) {
+    if (rows >= SIZE_MAX / sizeof(matrix_item) / cols)
+        throw MatrixException("Memory allocation error");
     if (this != &M) {
         delete[] data;
         rows = M.rows;
         cols = M.cols;
         data = new matrix_item[rows * cols];
+        if (data == nullptr)
+            throw MatrixException("Memory allocation error");
         std::copy(M.data, M.data + rows * cols, data);
     }
     return *this;
 }
 
 
-Matrix Matrix::operator+(const Matrix &M) const {
-    if (rows != M.rows || cols != M.cols) {
-        error(COLS_ROWS_ERROR);
-        return *this;
+Matrix &Matrix::operator=(Matrix &&M) noexcept {
+    if (this != &M) {
+        delete[] data;
+        rows = M.rows;
+        cols = M.cols;
+        data = M.data;
+        M.rows = 0;
+        M.cols = 0;
+        M.data = nullptr;
     }
+    return *this;
+}
+
+
+Matrix Matrix::operator+(const Matrix &M) const {
+    if (rows != M.rows || cols != M.cols)
+        throw MatrixException("Wrong number of columns or rows");
 
     Matrix sum = {rows, cols, UNFILLED};
-    for (size_t idx = 0; idx < rows * cols; idx++)
-        sum.data[idx] = data[idx] + M.data[idx];
+    if (sum.data != nullptr)
+        for (size_t idx = 0; idx < rows * cols; idx++)
+            sum.data[idx] = this->data[idx] + M.data[idx];
     return sum;
 }
 
 
 Matrix Matrix::operator-(const Matrix &M) const {
-    if (rows != M.rows || cols != M.cols) {
-        error(COLS_ROWS_ERROR);
-        return *this;
-    }
+    if (rows != M.rows || cols != M.cols)
+        throw MatrixException("Wrong number of columns or rows");
 
     Matrix sub = {rows, cols, UNFILLED};
-    for (size_t idx = 0; idx < rows * cols; idx++)
-        sub.data[idx] = data[idx] - M.data[idx];
+    if (sub.data != nullptr)
+        for (size_t idx = 0; idx < rows * cols; idx++)
+            sub.data[idx] = this->data[idx] - M.data[idx];
     return sub;
 }
 
 
 Matrix Matrix::operator*(double scalar) const {
     Matrix product = {rows, cols, UNFILLED};
-    for (size_t idx = 0; idx < rows * cols; idx++)
-        product.data[idx] = data[idx] * scalar;
+    if (product.data != nullptr)
+        for (size_t idx = 0; idx < rows * cols; idx++)
+            product.data[idx] = data[idx] * scalar;
     return product;
 }
 
 
 Matrix Matrix::operator*(const Matrix &M) const {
     if (cols != M.rows)
-        error(COLS_ROWS_ERROR);
+        throw MatrixException("Wrong number of columns or rows");
 
-    Matrix product = {rows, cols, UNFILLED};
-    for (size_t this_row = 0; this_row < rows; this_row++)
-        for (size_t M_col = 0; M_col < M.cols; M_col++)
-            for (size_t idx = 0; idx < cols; idx++)
-                product.data[this_row * cols + M_col] += data[this_row * cols + idx] * M.data[idx * M.cols + M_col];
+    Matrix product = {rows, cols, ZEROS};
+    if (product.data != nullptr)
+        for (size_t this_row = 0; this_row < rows; this_row++)
+            for (size_t M_col = 0; M_col < M.cols; M_col++)
+                for (size_t idx = 0; idx < cols; idx++)
+                    product.data[this_row * cols + M_col] +=
+                            data[this_row * cols + idx] * M.data[idx * M.cols + M_col];
     return product;
 }
 
 
 Matrix Matrix::T() {
     Matrix transposed = {cols, rows, UNFILLED};
-    for (size_t row = 0; row < rows; row++)
-        for (size_t col = 0; col < cols; col++)
-            transposed.data[row * rows + col] = data[col * cols + row];
+    if (transposed.data != nullptr)
+        for (size_t row = 0; row < rows; row++)
+            for (size_t col = 0; col < cols; col++)
+                transposed.data[row * rows + col] = data[col * cols + row];
     return transposed;
 }
 
 
 double Matrix::det() {
-    if (cols != rows) {
-        error(COLS_ROWS_ERROR);
-        return 0.;
-    }
-
-    double det = 0.;
+    if (cols != rows)
+        throw MatrixException("Wrong number of columns or rows");
     if (cols == 1)
-        det = (double) data[0];
-    else if (cols == 2)
-        det = (double) (data[0] * data[3] - data[1] * data[2]);
-    else if (cols == 3)
-        det = (double) (data[0] * data[4] * data[8]) + \
-                       (data[1] * data[5] * data[6]) + \
-                       (data[2] * data[3] * data[7]) - \
-                       (data[2] * data[4] * data[6]) - \
-                       (data[0] * data[5] * data[7]) - \
-                       (data[1] * data[3] * data[8]);
-    else error(DET_ERROR);
-    return det;
+        return (double) data[0];
+    if (cols == 2)
+        return (double) (data[0] * data[3] - data[1] * data[2]);
+    if (cols == 3)
+        return (double) (data[0] * data[4] * data[8]) + \
+                   (data[1] * data[5] * data[6]) + \
+                   (data[2] * data[3] * data[7]) - \
+                   (data[2] * data[4] * data[6]) - \
+                   (data[0] * data[5] * data[7]) - \
+                   (data[1] * data[3] * data[8]);
+    throw MatrixException("Determinant can be only calculated for 1x1, 2x2 or 3x3 matrices");
 }
 
 
-Matrix Matrix::exp(int n) {
-    if (cols != rows) error(COLS_ROWS_ERROR);
-    if (n < 1) error(EXP_LEVEl_ERROR);
-    if (n == 1) return *this;
+unsigned long long Matrix::factorial(unsigned int value) {
+    unsigned long long factorial = 1;
+    for (unsigned int idx = 1; idx <= value; idx++)
+        factorial *= idx;
+    return factorial;
+}
 
-    Matrix exponent(rows, cols, IDENTITY);
-    Matrix base = *this;
-    while (n > 0) {
-        if (n % 2 == 1) {
-            exponent = exponent * base;
-            base = base * base;
-            n = (n - 1) / 2;
-        } else {
-            base = base * base;
-            n /= 2;
+
+Matrix Matrix::exp(unsigned int n) const {
+    if (cols != rows) throw MatrixException("Wrong number of columns or rows");
+    if (data == nullptr) throw MatrixException("Bad matrix error");
+
+    if (n >= 1) {
+        Matrix exponent{rows, cols, IDENTITY};
+        Matrix pow{rows, cols, IDENTITY};
+        Matrix summand{rows, cols, UNFILLED};
+        if (exponent.data != nullptr && pow.data != nullptr && summand.data != nullptr) {
+            for (unsigned int idx = 1; idx <= n; idx++) {
+                pow = pow * (*this);
+                summand = pow * (1 / (double) factorial(idx));
+                exponent = exponent + summand;
+            }
         }
+        return exponent;
     }
-
-    return exponent;
+    throw MatrixException("Exponential level cannot be smaller than 1s");
 }
--- Makarov.Georgy/3_MatrixOOP/lib/libmatrix/src/libmatrix.h
+++ Makarov.Georgy/3_MatrixOOP/lib/libmatrix/src/libmatrix.h
@@ -5,6 +5,7 @@
 #include <iostream>
 #include <random>
 #include <iomanip>
+#include <cmath>
 
 typedef double matrix_item;
 
@@ -15,8 +16,13 @@ std::default_random_engine random_engine(42);
 enum MatrixType {
     ZEROS, ONES, RANDOM, IDENTITY, UNFILLED
 };
-enum ErrorType {
-    MEMORY_ERROR, BAD_MATRIX_ERROR, COLS_ROWS_ERROR, DET_ERROR, EXP_LEVEl_ERROR
+
+
+class MatrixException : public std::exception {
+    std::string _msg;
+public:
+    explicit MatrixException(std::string msg) : _msg(std::move(msg)) {}
+    const char *what() const noexcept override { return _msg.c_str(); }
 };
 
 
@@ -25,43 +31,28 @@ private:
     size_t rows{0};
     size_t cols{0};
     matrix_item *data{nullptr};
-
-
-    static void error(enum ErrorType error_type);
-
     void fill(enum MatrixType matrix_type);
 
 public:
+    Matrix() = default;
+    explicit Matrix(unsigned int n = 3);
     Matrix(size_t rows_amount, size_t cols_amount, MatrixType matrix_type);
-
     matrix_item get(size_t row, size_t col);
-
     void set(size_t row, size_t col, matrix_item item);
-
     void print();
-
-
     Matrix(const Matrix &M);
-
+    Matrix(Matrix &&M) noexcept;
     Matrix &operator=(const Matrix &M);
-
-
+    Matrix &operator=(Matrix &&M) noexcept;
     Matrix operator+(const Matrix &M) const;
-
     Matrix operator-(const Matrix &M) const;
-
     Matrix operator*(double scalar) const;
-
     Matrix operator*(const Matrix &M) const;
-
-
     Matrix T();
-
     double det();
-
-    Matrix exp(int n);
-
-    ~Matrix() { delete[] this->data; }
+    static unsigned long long factorial(unsigned int value);
+    Matrix exp(unsigned int n) const;
+    ~Matrix() { delete[] data; }
 };
 
 #endif //LIBMATRIX_H
