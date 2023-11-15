9f7b29d23cd926f8191c11f877f21ae11c9540b1
anonymous210370 <144235931+anonymous210370@users.noreply.github.com>
2023-11-15 16:18:00 +0300

    Update Matrix.c

--- Andreev.Alexander/Matrix.c
+++ Andreev.Alexander/Matrix.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <math.h>
 #include <string.h>
+#include <limits.h>
 
 typedef struct Matrix {
     size_t cols;
@@ -11,7 +12,16 @@ typedef struct Matrix {
 
 const Matrix MATRIX_NULL = { 0, 0, NULL };
 
+void matrix_error(char* error_message) {
+    printf("Error: %s\n", error_message);
+}
+
 Matrix matrix_init(const size_t cols, const size_t rows) {
+    if (rows >=  SIZE_MAX / sizeof(double) / cols) {
+        matrix_error("Failed to allocate memory for matrix: size too large")
+        return MATRIX_NULL;
+    }
+
     Matrix matrix;
     matrix.cols = cols;
     matrix.rows = rows;
@@ -19,14 +29,14 @@ Matrix matrix_init(const size_t cols, const size_t rows) {
     matrix.values = malloc(n_values * sizeof(double));
 
     if (matrix.values == NULL) {
-        printf("Failed to allocate memory for matrix\n");
+        matrix_error("Failed to allocate memory for matrix");
         return MATRIX_NULL;
     }
 
     return matrix;
 }
 
-void matrix_set_values(Matrix* matrix) {
+void matrix_set_values_random(Matrix* matrix) {
     if (matrix == NULL || matrix->values == NULL) {
         printf("Matrix is not properly initialized\n");
         return;
@@ -37,9 +47,6 @@ void matrix_set_values(Matrix* matrix) {
     }
 }
 
-void matrix_error_size(char* operation_name, char* error) {
-    printf("%s is impossible. %s\n", operation_name, error);
-}
 
 int matrix_is_null(const Matrix matrix) {
     return matrix.cols == 0 && matrix.rows == 0 ? 1 : 0;
@@ -66,7 +73,7 @@ void matrix_free(Matrix* matrix) {
 
 Matrix matrix_add(const Matrix matrix1, const Matrix matrix2) {
     if (matrix1.rows != matrix2.rows || matrix1.cols != matrix2.cols) {
-        matrix_error_size("Addition", "Matrixes should have equal sizes");
+        matrix_error("Matrix addition is impossible. Matrices should have equal sizes");
         return MATRIX_NULL;
     }
 
@@ -86,8 +93,8 @@ Matrix matrix_add(const Matrix matrix1, const Matrix matrix2) {
 }
 
 Matrix matrix_subtract(const Matrix matrix1, const Matrix matrix2) {
-    if (matrix1.rows != matrix2.rows && matrix1.cols != matrix2.cols) {
-        matrix_error_size("Subtraction", "Matrixes should have equal sizes");
+    if (matrix1.rows != matrix2.rows || matrix1.cols != matrix2.cols) {
+        matrix_error("Matrix subtraction is impossible. Matrices should have equal sizes");
         return MATRIX_NULL;
     }
 
@@ -124,7 +131,7 @@ Matrix matrix_multiply_by_double(const Matrix matrix, double number) {
 
 Matrix matrix_multiply(const Matrix A, const Matrix B) {
     if (A.cols != B.rows) {
-        matrix_error_size("Multiplication", "Matrix A cols should be equal to Matrix B rows");
+        matrix_error("Matrix multiplication is impossible. Matrix A cols should be equal to Matrix B rows");
         return MATRIX_NULL;
     }
 
@@ -148,8 +155,8 @@ Matrix matrix_multiply(const Matrix A, const Matrix B) {
 
 double matrix_det(const Matrix matrix) {
     if (matrix.cols != matrix.rows) {
-        matrix_error_size("Getting determinant", "Matrix should be square");
-        return 0.0;
+        matrix_error("Getting determinant is impossible. Matrix should be square");
+        return NAN;
     }
 
     double result = 0.0;
@@ -166,7 +173,7 @@ double matrix_det(const Matrix matrix) {
 
         if (submatrix.values == NULL) {
             matrix_free(&submatrix);
-            return 0.0;
+            return NAN;
         }
 
         size_t row_offset = 0;
@@ -227,7 +234,7 @@ Matrix matrix_identity(size_t dimension) {
 
 Matrix matrix_invertible(const Matrix matrix) {
     if (matrix.cols != matrix.rows) {
-        matrix_error_size("Getting invertible matrix", "Matrix should be square");
+        matrix_error("Getting invertible matrix is impossible. Matrix should be square");
         return MATRIX_NULL;
     }
 
@@ -279,6 +286,12 @@ Matrix matrix_invertible(const Matrix matrix) {
 
     matrix_free(&transponent);
 
+    if (matrix_det(result) == 0) {
+        matrix_error("Matrix is not invertible. Determinant is zero.");
+        matrix_free(&result);
+        return MATRIX_NULL;
+    }
+
     Matrix multiplied_result = matrix_multiply_by_double(result, 1 / matrix_det(result));
     matrix_free(&result);
 
@@ -299,7 +312,7 @@ Matrix matrix_copy(const Matrix matrix) {
 
 Matrix matrix_exp(const Matrix matrix, int accuracy) {
     if (matrix.cols != matrix.rows) {
-        matrix_error_size("Exp", "Matrix should be square");
+        matrix_error("Matrix exponential is impossible. Matrix should be square");
         return MATRIX_NULL;
     }
 
@@ -313,6 +326,13 @@ Matrix matrix_exp(const Matrix matrix, int accuracy) {
 
         new_powered = matrix_multiply(powered, matrix);
         matrix_free(&powered);
+
+        if (new_powered.values == NULL) {
+            matrix_error("Failed to compute matrix power");
+            matrix_free(&result);
+            return MATRIX_NULL;
+        }
+
         powered = matrix_copy(new_powered);
         matrix_free(&new_powered);
 
@@ -330,70 +350,75 @@ Matrix matrix_exp(const Matrix matrix, int accuracy) {
 }
 
 int main() {
-    Matrix m1, m2;
+    Matrix matrix1, matrix2;
 
-    m1 = matrix_init(3, 3);
-    matrix_set_values(&m1);
+    matrix1 = matrix_init(3, 3);
+    matrix_set_values_random(&matrix1);
     printf("\nMatrix 1\n");
-    matrix_print(m1);
+    matrix_print(matrix1);
 
-    m2 = matrix_init(3, 3);
-    matrix_set_values(&m2);
+    matrix2 = matrix_init(3, 3);
+    matrix_set_values_random(&matrix2);
     printf("Matrix 2\n");
-    matrix_print(m2);
+    matrix_print(matrix2);
 
     Matrix addition;
-    addition = matrix_add(m1, m2);
+    addition = matrix_add(matrix1, matrix2);
     printf("\nMatrix addition\n");
     matrix_print(addition);
     matrix_free(&addition);
 
     Matrix subtraction;
-    subtraction = matrix_subtract(m1, m2);
+    subtraction = matrix_subtract(matrix1, matrix2);
     printf("Matrix subtraction\n");
     matrix_print(subtraction);
     matrix_free(&subtraction);
 
     double determinant;
-    determinant = matrix_det(m1);
+    determinant = matrix_det(matrix1);
     printf("Matrix 1 determinant\n");
-    printf("%.2f\n\n", determinant);
-    determinant = matrix_det(m2);
+    if (!isnan(determinant)) {
+        printf("%.2f\n\n", determinant);
+    }
+
+    determinant = matrix_det(matrix2);
     printf("Matrix 2 determinant\n");
-    printf("%.2f\n\n", determinant);
+    if (!isnan(determinant)) {
+        printf("%.2f\n\n", determinant);
+    }
 
     Matrix multiplication1;
-    multiplication1 = matrix_multiply_by_double(m1, 5.);
+    multiplication1 = matrix_multiply_by_double(matrix1, 5.);
     printf("Matrix 1 multiplication\n");
     matrix_print(multiplication1);
     matrix_free(&multiplication1);
 
     Matrix multiplication2;
-    multiplication2 = matrix_multiply(m1, m2);
+    multiplication2 = matrix_multiply(matrix1, matrix2);
     printf("Matrix 1 and 2 multiplication\n");
     matrix_print(multiplication2);
     matrix_free(&multiplication2);
 
-    Matrix trans;
-    trans = matrix_transpose(m1);
+    Matrix transposition;
+    transposition = matrix_transpose(matrix1);
     printf("Matrix 1 transposed\n");
-    matrix_print(trans);
-    matrix_free(&trans);
+    matrix_print(transposition);
+    matrix_free(&transposition);
 
-    Matrix invert;
-    invert = matrix_invertible(m1);
+    Matrix invertible_matrix;
+    invertible_matrix = matrix_invertible(matrix1);
     printf("Matrix 1 inverted\n");
-    matrix_print(invert);
-    matrix_free(&invert);
+    matrix_print(invertible_matrix);
+    matrix_free(&invertible_matrix);
 
-    Matrix exponent;
-    exponent = matrix_exp(m1, 3);
+    Matrix exponential;
+    exponential = matrix_exp(matrix1, 3);
     printf("Matrix 1 exponential\n");
-    matrix_print(exponent);
-    matrix_free(&exponent);
+    matrix_print(exponential);
+    matrix_free(&exponential);
 
-    matrix_free(&m1);
-    matrix_free(&m2);
+    matrix_free(&matrix1);
+    matrix_free(&matrix2);
 
     return 0;
 }
