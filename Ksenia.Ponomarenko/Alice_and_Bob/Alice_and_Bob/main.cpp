c317a2ba5494c4500564cffa3fbe2cc37acea90d
Ксения Пономаренко <ponomarenko.ka@edu.spbstu.ru>
2023-11-23 01:48:23 +0300

    Alice and Bob upd

--- Ksenia.Ponomarenko/Alice_and_Bob/Alice_and_Bob/main.cpp
+++ Ksenia.Ponomarenko/Alice_and_Bob/Alice_and_Bob/main.cpp
@@ -3,45 +3,45 @@
 
 const double INFL = 0.07;
 
-double platezh_bob(double ipoteka, int year, int kvar, int start)
+double payment_bob(double mortgage, int year, int flat, int start_capital)
 {
-	int S = kvar - start; // —умма кредита
-	int n = year * 12; //  ол-во мес€цев во всем сроке кредита
-	float r = ipoteka / 12; // ћес€чна€ ставка
-	double count = pow((1 + r), (float)n); // ¬озводим (1+r) в степень n, попутно перевод€ n из int в float
-	double platezh = S * ((r * count) / (count - 1)); // —умма ежемес€чного платежа по ипотеке по формуле
+	int S = flat - start_capital; // Credit sum
+	int n = year * 12; // Mounth count while credit
+	float r = mortgage / 12; // Mounth rate
+	double count = pow((1 + r), (float)n);
+	double platezh = S * ((r * count) / (count - 1)); // Sum of annually payment during mortgage
 	return platezh;
 };
 
-double kvartira_bob(double kvartira, int year )
+double flat_bob(double flat, int year )
 {
-	kvartira = kvartira * (powf(1 + INFL, year));
-	return kvartira;
+	flat = flat * (powf(1 + INFL, year));
+	return flat;
 };
 
 
-double capital(double zp, double traty, double platezh, double  remont, double bank_pp, double cap)
+double capital(double salary, double expenditure, double payment, double  repair, double bank_pp, double capital)
 {
 
-	double proc_mes = bank_pp / 12;
-	double zp_itog;
+	double percent_mounth = bank_pp / 12;
+	double salary_total;
 	for (int i = 1; i < 360; i++)
 	{ 
-		zp_itog = zp * powf(1 + INFL, i / 12);
-		cap += zp_itog;
+		salary_total = salary * powf(1 + INFL, i / 12);
+		capital += salary_total;
 		if (i >= 60)
 		{
-			remont = 0;
+			repair = 0;
 		}
-		cap = (cap - platezh - ((traty + remont) * powf(1 + (INFL / 12), i))) * (1 + (bank_pp / 12));
+		capital = (capital - payment - ((expenditure + repair) * powf(1 + (INFL / 12), i))) * (1 + (bank_pp / 12));
 	}
-	return cap;
+	return capital;
 };
 
 int main()
 {
-	double platezh = platezh_bob(0.07, 30, 20000000, 1000000);
-	double bob_capital = capital(200000.0, 50000.0, platezh, 10000.0, 0.11, 0.0) + kvartira_bob(20000000.0,30);
+	double payment = payment_bob(0.07, 30, 20000000, 1000000);
+	double bob_capital = capital(200000.0, 50000.0, payment, 10000.0, 0.11, 0.0) + flat_bob(20000000.0,30);
 	printf("\nBob's capital: ");
 	double alice_capital = capital(200000.0, 50000.0 + 30000.0, 0.0, 0.0, 0.11, 1000000.0);
 	printf("%f", bob_capital);
