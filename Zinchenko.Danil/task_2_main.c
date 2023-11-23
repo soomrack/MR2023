57faa0f43114a164005a1e27484c89e15706d9e4
Ermacovich <46522819+Ermacovich@users.noreply.github.com>
2023-11-23 10:38:06 +0300

    Update task_2_main.c

--- Zinchenko.Danil/task_2_main.c
+++ Zinchenko.Danil/task_2_main.c
@@ -1,1 +1,9 @@
+#include <stdio.h>
+#include "..\task_2_lib.c\task_2_lib.c.h";
 
+int main()
+{
+    struct Matrix A = matrix_create(3, 3);
+    matrix_set_one(A);
+    matrix_print(A, "Matrix A");
+}
