43393f2f75aa8cc31afa822199e5cf4c48090bfa
Mikhail Ananyevskiy <msaipme@gmail.com>
2023-11-23 10:42:24 +0300

    notes

--- Ksenia.Ponomarenko/Alice_and_Bob/Alice_and_Bob/main.cpp
+++ Ksenia.Ponomarenko/Alice_and_Bob/Alice_and_Bob/main.cpp
@@ -1,7 +1,15 @@
 #include <stdio.h>
 #include <math.h>
 
+typedef long long int Money;  // копейки
+
 const double INFL = 0.07;
+
+
+struct Person {
+    Money capital;    
+};
+
 
 double payment_bob(double mortgage, int year, int flat, int start_capital)
 {
@@ -9,9 +17,10 @@ double payment_bob(double mortgage, int year, int flat, int start_capital)
 	int n = year * 12; // Mounth count while credit
 	float r = mortgage / 12; // Mounth rate
 	double count = pow((1 + r), (float)n);
-	double payment = S * ((r * count) / (count - 1)); // Sum of annually payment during mortgage
+	double payment = S * ((r * count) / (count - 1));  // Sum of annually payment during mortgage
 	return payment;
 };
+
 
 double flat_bob(double flat, int year )
 {
@@ -25,12 +34,10 @@ double capital(double salary, double expenditure, double payment, double  repair
 
 	double percent_mounth = bank_pp / 12;
 	double salary_total;
-	for (int i = 1; i < 360; i++)
-	{ 
+	for (int i = 1; i < 360; i++) { 
 		salary_total = salary * powf(1 + INFL, i / 12);
 		capital += salary_total;
-		if (i >= 60)
-		{
+		if (i >= 60) {
 			repair = 0;
 		}
 		capital = (capital - payment - ((expenditure + repair) * powf(1 + (INFL / 12), i))) * (1 + (bank_pp / 12));
@@ -38,11 +45,25 @@ double capital(double salary, double expenditure, double payment, double  repair
 	return capital;
 };
 
+
+void simulation() {
+    for (...) {
+        bob_salary(year, month);
+        bob_food(year, month);
+        bob_mortgage(year, month);
+        
+        
+    }
+}
+
+
+
 int main()
 {
 	double payment = payment_bob(0.07, 30, 20000000, 1000000);
 	double bob_capital = capital(200000.0, 50000.0, payment, 10000.0, 0.11, 0.0) + flat_bob(20000000.0,30);
 	printf("\nBob's capital: ");
+
 	double alice_capital = capital(200000.0, 50000.0 + 30000.0, 0.0, 0.0, 0.11, 1000000.0);
 	printf("%f", bob_capital);
 	printf("\nAlice's capital: ");
