12a341854bd5c7e86a870cf02850fb3f29a36be5
Ксения Пономаренко <ponomarenko.ka@edu.spbstu.ru>
2023-11-25 16:58:48 +0300

    Matrix

---
+++ Ksenia.Ponomarenko/Matrix/Main.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include "..\Matrix_lib\Matrix_lib.h";
+
+int main()
+{
+    struct Matrix A = matrix_allocation(3, 3);
+    matrix_set_zero(A);
+    matrix_print(A, "Matrix b");
+}
