60b522480c1c60ce8354ed090701744d40a6a857
anonymous210370 <144235931+anonymous210370@users.noreply.github.com>
2023-11-30 11:14:53 +0300

    Final Final Final Final

--- Andreev.Alexander/Matrix.cpp
+++ Andreev.Alexander/Matrix.cpp
@@ -1,418 +1,330 @@
-﻿#include <iostream>
-#include <string>
-#include <cstring>
-#include <vector>
-#include <random>
+﻿#pragma once
+#include <ctime>
+#include <iostream>
+#include <windows.h>
+#include <cmath>
+#include <stdexcept>
 
-typedef double MatrixItem;
-enum MatrixType { ZERO, ONES, RANDOM, I };
+const double EPS = 1e-6;
 
-class Matrix_Exception : public std::exception {
-private:
-    std::string message;
-
+class MatrixException : public std::domain_error {
 public:
-    Matrix_Exception(std::string message) : message{ message } {}
-    const char* what() const noexcept override { return message.c_str(); }
+    MatrixException(const char* const& msg) : domain_error(msg) {}
 };
+
+MatrixException notSquare("Error: the matrix must be square (should have nxn size)\n");
+MatrixException notEqual("Error: the matrix should have the same size\n");
+MatrixException multiplyError("Error: first matrix cols not equal to the second matrix row.\n");
+MatrixException zeroDivision("Error: divide by zero\n");
+MatrixException memError("Error: memory is not allocated\n");
+MatrixException emptyMatrix("Error: Function can't return an empty matrix\n");
+MatrixException sizeError("Error: size error\n");
 
 class Matrix {
 private:
     size_t rows;
     size_t cols;
-    MatrixItem* data;
+    double* data;
 
 public:
+    Matrix(size_t rows, size_t cols, double* data);
+    Matrix(size_t rows, size_t cols);
     Matrix();
-    Matrix(const size_t n);
-    Matrix(const size_t row, const size_t col);
-    Matrix(const Matrix& M);
-    Matrix(Matrix&& M) noexcept;
     ~Matrix();
+    Matrix(const Matrix& a);
+    Matrix(Matrix&& a) noexcept;
 
 public:
-    Matrix& operator= (const Matrix& M);
-    Matrix& operator+= (const Matrix& M);
-    Matrix& operator-= (const Matrix& M);
-    Matrix& operator*= (const double k);
-    Matrix& operator*= (const Matrix& M);
+    void matrixPrint();
 
-    Matrix operator+(const Matrix& K) const;
-    Matrix operator-(const Matrix& K) const;
-    Matrix operator*(const Matrix& K) const;
-    Matrix operator*(const double k) const;
+    size_t getRows();
+    size_t getCols();
+    double* getData();
 
-public:
-    void print();
-    void set(MatrixType mat_type);
+    void setZero();
+    void setIdentity();
     void transpose();
-    double determinant();
-    void exp(unsigned int level);
-};
+    Matrix exponent(const size_t step = 100);
+    Matrix minor(const unsigned int minorRow, const unsigned int minorCol);
+    Matrix reverse(const Matrix* matrix);
 
-Matrix operator*(const double k, const Matrix& M);
+    double determinant(const Matrix* matrix);
+
+    Matrix& operator=(Matrix&& a) noexcept;
+    Matrix& operator=(const Matrix& a);
+    void operator+=(const Matrix& a);
+    void operator-=(const Matrix& a);
+    void operator*=(const double k);
+    void operator/=(const double k);
+    void operator*=(const Matrix& a);
+
+    Matrix operator+(const Matrix& a);
+    Matrix operator-(const Matrix& a);
+    Matrix operator*(const Matrix& a);
+    Matrix operator*(const double k);
+    Matrix operator/(const double k);
+
+    bool operator!=(const Matrix& a);
+    bool operator==(const Matrix& a);
+    friend std::ostream& operator<<(std::ostream& out, const Matrix);
+};
 
 Matrix::Matrix() : rows(0), cols(0), data(nullptr) {}
 
-Matrix::Matrix(const size_t n) : rows(n), cols(n) {
-    if (n == 0) {
-        throw Matrix_Exception("Incorrect input matrix: size cannot be zero");
+Matrix::Matrix(const Matrix& matrix) {
+    rows = matrix.rows;
+    cols = matrix.cols;
+    data = new double[rows * cols];
+    memcpy(data, matrix.data, rows * cols * sizeof(double));
+}
+
+Matrix::Matrix(size_t numRows, size_t numCols) {
+    if (numRows >= SIZE_MAX / sizeof(double) / numCols) {
+        throw sizeError;
     }
-
-    data = new MatrixItem[rows * cols]{};
+    rows = numRows;
+    cols = numCols;
+    data = new double[rows * cols];
 }
 
-Matrix::Matrix(const size_t row, const size_t col) : rows(row), cols(col) {
-    if (row == 0 || col == 0) {
-        throw Matrix_Exception("Incorrect input matrix: size cannot be zero");
-    }
-
-    data = new MatrixItem[rows * cols]{};
+Matrix::Matrix(size_t numRows, size_t numCols, double* array) {
+    rows = numRows;
+    cols = numCols;
+    data = new double[rows * cols];
+    memcpy(data, array, rows * cols * sizeof(double));
 }
 
-Matrix::Matrix(const Matrix& M) : rows(M.rows), cols(M.cols) {
-    if (M.data) {
-        data = new MatrixItem[rows * cols]{};
-        std::copy(M.data, M.data + rows * cols, data);
-    }
-    else {
-        throw Matrix_Exception("Incorrect input matrix: source matrix is empty");
-    }
+Matrix::Matrix(Matrix&& mat) noexcept : data(mat.data), rows(mat.rows), cols(mat.cols) {
+    mat.data = nullptr;
+    mat.rows = 0;
+    mat.cols = 0;
 }
 
-Matrix::Matrix(Matrix&& M) noexcept : rows(M.rows), cols(M.cols), data(M.data) {
-    M.rows = 0;
-    M.cols = 0;
-    M.data = nullptr;
-}
-
-Matrix::~Matrix() {
-    delete[] data;
-}
-
-Matrix& Matrix::operator= (const Matrix& M) {
-    if (this == &M) return *this;
-
-    if (M.rows == 0 || M.cols == 0) {
-        throw Matrix_Exception("Incorrect input matrix: source matrix is empty");
-    }
-
-    rows = M.rows;
-    cols = M.cols;
-    delete[] data;
-    data = new MatrixItem[rows * cols]{};
-    std::copy(M.data, M.data + rows * cols, data);
-    return *this;
-}
-
-Matrix& Matrix::operator+= (const Matrix& M) {
-    if ((rows != M.rows) or (cols != M.cols))
-        throw Matrix_Exception("The matrix has an incorrect dimension");
-
-    if (data && M.data) {
-        for (size_t idx = 0; idx < rows * cols; idx++) {
-            data[idx] += M.data[idx];
+std::ostream& operator<<(std::ostream& out, const Matrix matrix) {
+    for (size_t row = 0; row < matrix.rows; row++) {
+        for (size_t col = 0; col < matrix.cols; col++) {
+            out << matrix.data[row * matrix.cols + col] << "\t";
         }
+        out << std::endl;
     }
-    else {
-        throw Matrix_Exception("Your matrix is empty");
-    }
-
-    return *this;
+    out << std::endl;
+    return (out);
 }
 
-Matrix& Matrix::operator-= (const Matrix& M) {
-    if ((rows != M.rows) or (cols != M.cols))
-        throw Matrix_Exception("The matrix has an incorrect dimension");
-
-    if (data && M.data) {
-        for (size_t idx = 0; idx < rows * cols; idx++) {
-            data[idx] -= M.data[idx];
-        }
+bool Matrix::operator==(const Matrix& a, double eps) {
+    if (cols != a.cols || rows != a.rows)
+        return false;
+    for (unsigned int idx = 0; idx < cols * rows; idx++) {
+        if (abs(data[idx] - a.data[idx]) > eps)
+            return false;
     }
-    else {
-        throw Matrix_Exception("Your matrix is empty");
-    }
-
-    return *this;
+    return true;
 }
 
-Matrix& Matrix::operator*= (const double k) {
-    if (data) {
-        for (size_t idx = 0; idx < rows * cols; idx++) {
-            data[idx] *= k;
-        }
-    }
-    else {
-        throw Matrix_Exception("Your matrix is empty");
-    }
 
-    return *this;
+void Matrix::setZero() {
+    if (data != nullptr)
+        memset(data, 0, rows * cols * sizeof(double));
 }
 
-Matrix& Matrix::operator*= (const Matrix& M) {
-    if (cols != M.rows)
-        throw Matrix_Exception("The matrix has an incorrect dimension");
-
-    Matrix R(rows, M.cols);
-    R.set(ZERO);
-
-    if (data && M.data) {
-        for (size_t row = 0; row < R.rows; row++)
-            for (size_t col = 0; col < R.cols; col++)
-                for (size_t idx = 0; idx < M.rows; idx++)
-                    R.data[row * R.cols + col] += data[row * cols + idx] * M.data[idx * M.cols + col];
-
-        cols = R.cols;
-        rows = R.rows;
-        delete[] data;
-        data = R.data;
-        R.data = nullptr;
-    }
-    else {
-        throw Matrix_Exception("Your matrix or the source matrix is empty");
-    }
-
-    return *this;
-}
-
-Matrix Matrix::operator+(const Matrix& K) const {
-    Matrix result = *this;
-    result += K;
-    return result;
-}
-
-Matrix Matrix::operator-(const Matrix& K) const {
-    Matrix result = *this;
-    result -= K;
-    return result;
-}
-
-Matrix Matrix::operator*(const Matrix& K) const {
-    Matrix result = *this;
-    result *= K;
-    return result;
-}
-
-Matrix Matrix::operator*(const double k) const {
-    Matrix result = *this;
-    result *= k;
-    return result;
-}
-
-Matrix operator*(const double k, const Matrix& M) {
-    Matrix result = M;
-    result *= k;
-    return result;
-}
-
-void Matrix::print() {
-    if (!data) throw Matrix_Exception("Your matrix is empty");
-
-    std::cout << "\n";
-    for (size_t r = 0; r < rows; r++) {
-        std::cout << "[";
-        for (size_t c = 0; c < cols; c++) {
-            std::cout << data[r * cols + c];
-            if (c != cols - 1)
-                std::cout << "\t";
-        }
-        std::cout << "]\n";
-    }
-    std::cout << "\n";
-}
-
-void Matrix::set(MatrixType mat_type) {
-    if (!data) throw Matrix_Exception("Your matrix is empty");
-
-    switch (mat_type) {
-    case (ZERO):
-        std::fill(data, data + cols * rows, 0.0);
-        break;
-
-    case (ONES):
-        std::fill(data, data + cols * rows, 1.0);
-        break;
-
-    case (RANDOM): {
-        std::random_device rd;
-        std::mt19937 gen(rd());
-        std::uniform_real_distribution<double> dis(0.0, 10.0);
-
-        for (size_t idx = 0; idx < cols * rows; idx++)
-            data[idx] = dis(gen);
-        break;
-    }
-
-    case (I):
-        set(ZERO);
-        for (size_t row_col = 0; row_col < rows; row_col++)
-            data[row_col * cols + row_col] = 1;
-        break;
+void Matrix::setIdentity() {
+    setZero();
+    for (unsigned int idx = 0; idx < cols * rows; idx += rows + 1) {
+        data[idx] = 1.0;
     }
 }
 
 void Matrix::transpose() {
-    if (!data) throw Matrix_Exception("Your matrix is empty");
-
-    MatrixItem* newData = new MatrixItem[rows * cols]{};
-
-    for (size_t row = 0; row < rows; row++)
-        for (size_t col = 0; col < cols; col++)
-            newData[row * cols + col] = data[col * rows + row];
-
-    delete[] data;
-    data = newData;
-    std::swap(rows, cols);
-}
-
-double Matrix::determinant() {
-    if (cols == 2) {
-        if (data) {
-            return data[0] * data[3] - data[1] * data[2];
-        }
-        else {
-            throw Matrix_Exception("Your matrix is empty");
+    Matrix trans(cols, rows);
+    for (unsigned int row = 0; row < trans.rows; row++) {
+        for (unsigned int col = 0; col < trans.cols; col++) {
+            trans.data[row * trans.cols + col] = data[col * trans.rows + row];
         }
     }
-
-    if (cols == 3) {
-        if (data) {
-            return (data[0] * data[4] * data[8]) +
-                (data[1] * data[5] * data[6]) +
-                (data[2] * data[3] * data[7]) -
-                (data[2] * data[4] * data[6]) -
-                (data[0] * data[5] * data[7]) -
-                (data[1] * data[3] * data[8]);
-        }
-        else {
-            throw Matrix_Exception("Your matrix is empty");
-        }
-    }
-
-    throw Matrix_Exception("Incorrect input matrix");
+    *this = trans;
 }
 
-void Matrix::exp(unsigned int level) {
-    if (!data) throw Matrix_Exception("Your matrix is empty");
-
-    Matrix R(rows, cols);
-    R.set(I);
-
-    double n = 1.0;
-    Matrix temp = R;
-    for (unsigned int idx = 1; idx < level; idx++) {
-        temp *= *this;
-        n /= (idx);
-        R += (temp * n);
-    }
-    *this = R;
+size_t Matrix::getRows() {
+    return rows;
 }
 
-int main(void) {
-    try {
-        unsigned int choice, run = 1;
-        Matrix A(3), B(3, 3), C(4);
+size_t Matrix::getCols() {
+    return cols;
+}
 
-        while (run) {
-            std::cout << "\n"
-                "0 - exit\n"
-                "1 - set\n"
-                "2 - print A, B, C\n"
-                "3 - C = A\n"
-                "4 - C += A\n"
-                "5 - C -= A\n"
-                "6 - C *= 2.0\n"
-                "7 - C *= B\n"
-                "8 - C = A + B\n"
-                "9 - C = A - B\n"
-                "10 - C = A * 2\n"
-                "11 - C = A * B\n"
-                "12 - A.print()\n"
-                "13 - B.print()\n"
-                "14 - C.transpose()\n"
-                "15 - C.determinant()\n"
-                "16 - C.exp(20)\n"
-                << std::endl;
+Matrix& Matrix::minor(const unsigned int minorRow, const unsigned int minorCol) {
+    if (rows == 0 || cols == 0) {
+        throw emptyMatrix;
+    }
+    Matrix* minor = new Matrix(rows - 1, cols - 1);
+    unsigned int minorIndex = 0;
+    for (unsigned int row = 0; row < rows; row++) {
+        for (unsigned int col = 0; col < cols; col++) {
+            if (row == minorRow || col == minorCol)
+                continue;
+            minor->data[minorIndex++] = data[row * cols + col];
+        }
+    }
+    return *minor;
+}
 
-            std::cout << "choice: " << std::endl;
-            std::cin >> choice;
+void Matrix::operator+=(const Matrix& matrix) {
+    if (rows != matrix.rows || cols != matrix.cols)
+        throw notEqual;
+    for (unsigned int index = 0; index < rows * cols; ++index) {
+        data[index] += matrix.data[index];
+    }
+}
 
-            switch (choice) {
-            case 0:
-                run = 0;
-                break;
-            case 1:
-                A.set(RANDOM);
-                B.set(RANDOM);
-                C.set(I);
-                break;
-            case 2:
-                A.print();
-                B.print();
-                C.print();
-                break;
-            case 3:
-                C = A;
-                C.print();
-                break;
-            case 4:
-                C += A;
-                C.print();
-                break;
-            case 5:
-                C -= A;
-                C.print();
-                break;
-            case 6:
-                C *= 2.0;
-                C.print();
-                break;
-            case 7:
-                C *= B;
-                C.print();
-                break;
-            case 8:
-                C = A + B;
-                C.print();
-                break;
-            case 9:
-                C = A - B;
-                C.print();
-                break;
-            case 10:
-                C = A * 2;
-                C.print();
-                break;
-            case 11:
-                C = A * B;
-                C.print();
-                break;
-            case 12:
-                A.print();
-                break;
-            case 13:
-                B.print();
-                break;
-            case 14:
-                C.transpose();
-                C.print();
-                break;
-            case 15:
-                std::cout << C.determinant() << std::endl;
-                break;
-            case 16:
-                C.exp(20);
-                C.print();
-                break;
-            default:
-                printf("wrong choice\n");
-                break;
+void Matrix::operator-=(const Matrix& matrix) {
+    if (rows != matrix.rows || cols != matrix.cols)
+        throw notEqual;
+    for (unsigned int index = 0; index < rows * cols; ++index) {
+        data[index] -= matrix.data[index];
+    }
+}
+
+void Matrix::operator*=(const double k) {
+    for (unsigned int idx = 0; idx < rows * cols; idx++) {
+        data[idx] *= k;
+    }
+}
+
+void Matrix::operator/=(const double k) {
+    for (unsigned int idx = 0; idx < rows * cols; idx++) {
+        data[idx] /= k;
+    }
+}
+
+void Matrix::operator*=(const Matrix& matrix) {
+    if (cols != matrix.rows)
+        throw multiplyError;
+
+    Matrix multiplication(rows, matrix.cols);
+    for (unsigned int row = 0; row < multiplication.rows; row++) {
+        for (unsigned int col = 0; col < multiplication.cols; col++) {
+            double sum = 0;
+            for (unsigned int k = 0; k < matrix.rows; k++) {
+                sum += data[row * cols + k] * matrix.data[k * matrix.cols + col];
             }
+            multiplication.data[row * multiplication.cols + col] = sum;
         }
-        return 0;
     }
-    catch (const Matrix_Exception& exception) {
-        std::cout << exception.what() << std::endl;
+    *this = multiplication;
+}
+
+Matrix& Matrix::operator=(const Matrix& a) {
+    if (this == &a)
+        return *this;
+
+    if (rows * cols == a.rows * a.cols) {
+        rows = a.rows;
+        cols = a.cols;
+        memcpy(data, a.data, rows * cols * sizeof(double));
+        return *this;
     }
+    delete[] data;
+    rows = a.rows;
+    cols = a.cols;
+    data = new double[rows * cols];
+    memcpy(data, a.data, rows * cols * sizeof(double));
+    return *this;
+}
+
+Matrix& Matrix::operator=(Matrix&& a) noexcept {
+    delete[] data;
+    rows = a.rows;
+    cols = a.cols;
+    data = a.data;
+    a.data = nullptr;
+    a.rows = 0;
+    a.cols = 0;
+    return *this;
+}
+
+Matrix& Matrix::operator+(const Matrix& b) {
+    Matrix* add = new Matrix(*this);
+    *add += b;
+    return *add;
+}
+
+Matrix& Matrix::operator-(const Matrix& b) {
+    Matrix* sub = new Matrix(*this);
+    *sub -= b;
+    return *sub;
+}
+
+Matrix& Matrix::operator*(const double k) {
+    Matrix* multiply = new Matrix(*this);
+    *multiply *= k;
+    return *multiply;
+}
+
+Matrix& Matrix::operator*(const Matrix& b) {
+    Matrix* multiply = new Matrix(*this);
+    *multiply *= b;
+    return *multiply;
+}
+
+Matrix& Matrix::operator/(const double k) {
+    Matrix* multiply = new Matrix(*this);
+    *multiply /= k;
+    return *multiply;
+}
+
+double Matrix::determinant(const Matrix* matrix) {
+    if (matrix->rows != matrix->cols)
+        throw notSquare;
+    double det = 0;
+    int k = 1;
+    if (matrix->rows == 0)
+        return NAN;
+    if (matrix->rows == 1)
+        return matrix->data[0];
+    if (matrix->rows == 2) {
+        return (matrix->data[0] * matrix->data[3] - matrix->data[2] * matrix->data[1]);
+    }
+    for (unsigned int idx = 0; idx < matrix->cols; idx++) {
+        Matrix temp = minor(0, idx);
+        det += k * matrix->data[idx] * determinant(&temp);
+        k = -k;
+    }
+    return det;
+}
+
+Matrix& Matrix::reverse(const Matrix* matrix) {
+    double d = determinant(matrix);
+    if (matrix->rows != matrix->cols)
+        throw notSquare;
+    if (fabs(d) < EPS)
+        throw zeroDivision;
+    Matrix* reverse = new Matrix(matrix->rows, matrix->cols);
+    int k = 1;
+    for (unsigned int row = 0; row < reverse->rows; row++) {
+        for (unsigned int col = 0; col < reverse->cols; col++) {
+            Matrix temp = minor(row, col);
+            reverse->data[row * matrix->cols + col] = k * determinant(&temp);
+            k = -k;
+        }
+    }
+    reverse->transpose();
+    *reverse /= d;
+    return *reverse;
+}
+
+Matrix& Matrix::exponent(const size_t accuracy) {
+    if (cols != rows) {
+        throw notSquare;
+    }
+
+    Matrix sum = Matrix(rows, cols);
+    sum.setIdentity();
+
+    Matrix* exp = new Matrix(sum);
+    for (size_t degAcc = 1; degAcc <= accuracy; ++degAcc) {
+        sum = sum * (*this) / degAcc;
+        *exp += sum;
+    }
+
+    return *exp;
 }
