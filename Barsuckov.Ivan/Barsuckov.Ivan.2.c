3701b1169341827085f579d3cf29cb6249d8516e
rotcafeneb <102895719+rotcafeneb@users.noreply.github.com>
2023-11-30 11:45:12 +0300

    Update Barsuckov.Ivan.2.c

--- Barsuckov.Ivan/Barsuckov.Ivan.2.c
+++ Barsuckov.Ivan/Barsuckov.Ivan.2.c
@@ -1,7 +1,8 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
-
+#include <stdint.h>
+#include <math.h>
 
 struct Matrix {
     size_t cols;
@@ -14,7 +15,7 @@ const struct Matrix MATRIX_NULL = { .cols = 0, .rows = 0, .data = NULL };
 
 struct Matrix matrix_init(const size_t rows, const size_t cols)
 {
-    /*if ((rows * cols) < (max size_t / sizeof(double))) {
+   /* if ((rows * cols) < (SIZE_MAX / sizeof(double))) {
         return MATRIX_NULL;
     }*/
 
@@ -71,6 +72,13 @@ int matrix_add(struct Matrix* A, struct Matrix* B)
     return 0;
 }*/
 
+typedef double MatrixItem;
+
+void matrix_zero(struct Matrix A)
+{
+    memset(A.data, 0, A.cols * A.rows * sizeof(MatrixItem));
+    return;
+}
 
 struct Matrix matrix_sum(const struct Matrix A, const struct Matrix B)
 {
@@ -104,19 +112,20 @@ struct Matrix matrix_scalar_mult(const struct Matrix A)
     struct Matrix M = matrix_init(A.rows, B.cols);
     if (M.data == NULL)
         return M;
+
+    matrix_zero(M);
         
         for (size_t rowA = 0; rowA < A.rows; ++rowA) {
             for (size_t colB = 0; colB < B.cols; ++colB) {
                 for (size_t colAk = 0; colAk < A.cols; ++colAk) {
                     M.data[A.cols * rowA + colB] +=
-                        A.data[colAk + rowA * A.cols] * B.data[B.cols * colAk + colB];
+                        A.data[colAk + (rowA * A.cols)] * B.data[B.cols * colAk + colB];
                 }
             }
         }
 
     return M;
 }
-
 
 
  struct Matrix matrix_tr(const struct Matrix A, const struct Matrix B)
@@ -131,24 +140,47 @@ struct Matrix matrix_scalar_mult(const struct Matrix A)
  }
 
 
-
-
- /*struct Matrix matrix_exp(const struct Matrix A, const struct Matrix B)
+ /*struct Matrix matrix_exp(const struct Matrix A, const double accuracy)  // accuracy - десятичная дробь)
  {
-     struct Matrix E = matrix_init(A.rows, B.cols);
+     
+    if (A.cols != A.rows)
+        return MATRIX_NULL;
 
-     ...
+    struct Matrix E = matrix_init(A.rows, B.cols);
+    if (E.data == NULL)
+        return E;
+    matrix_zero(E);
 
-     return E;
- }*/
+    struct Matrix B = matrix_init(A.cols, A.rows);
+    matrix_copy(B, A);
 
+    int degree;
+    degree = (int)(ceil(1.0 / accuracy));
 
- /*// возвращается 0, если в матрице есть нулевые строки/столбцы
+    for (int trm = 2; trm <= degree; ++trm) {
+       matrix_add_mult(B, A);
+       matrix_mult_by_coeff(B, 1.0 / trm);
+       matrix_add(E, B);
+    };
+
+    matrix_add(E, A);
+
+    for (size_t diag = 0; diag < E.rows; ++diag)
+        E.data[diag * E.cols + diag] += 1;
+
+    matrix_free(&B);
+
+    return E;
+}
+*/
+
+ 
+ // проверяет наличие в матрице нулевых строк/столбцов (иначе возвращается 0)
  int matrix_det_if_zero(const struct Matrix A)
  {
      size_t count;
 
-     for (size_t row = 0; row < A.rows; ++row) { // суммы по строкам
+     for (size_t row = 0; row < A.rows; ++row) {  // суммы по строкам
          count = 0;
          for (size_t col = 0; col < A.cols; ++col) {
              count += 1;
@@ -160,7 +192,7 @@ struct Matrix matrix_scalar_mult(const struct Matrix A)
              return 0;
      };
 
-     for (size_t col = 0; col < A.cols; ++col) { // суммы по строкам
+     for (size_t col = 0; col < A.cols; ++col) {  // суммы по строкам
          count = 0;
          for (size_t row = 0; row < A.rows; ++row) {
              count += 1;
@@ -176,24 +208,24 @@ struct Matrix matrix_scalar_mult(const struct Matrix A)
  }
 
 
- // меняет строки местами, если первый символ в диагонали == 0
+ // меняет строки местами
  void matrix_det_prep(const struct Matrix A, size_t diag, double* coeff)
  {
-     size_t buff_one = diag; // запоминается номер строки
+     size_t row_number_buffer = diag;  // запоминается номер строки
 
      if (A.data[diag * A.cols + diag] == 0.0) {
          for (size_t row = diag; row < A.rows; ++row) {
-             buff_one += 1;
+             row_number_buffer += 1;
              if (A.data[row * A.cols] != 0.0)
                  break;
          };
 
-         double buff_two = 0; // запоминается значение в ячейке
+         double cell_value_buffer = 0;  // запоминается значение в ячейке
 
          for (size_t col = diag; col < A.cols; ++col) {
-             buff_two = A.data[diag * A.cols + col];
-             A.data[diag * A.cols + col] = A.data[buff_one * A.cols + col];
-             A.data[buff_one * A.cols + col] = buff_two;
+             cell_value_buffer = A.data[diag * A.cols + col];
+             A.data[diag * A.cols + col] = A.data[row_number_buffer * A.cols + col];
+             A.data[row_number_buffer * A.cols + col] = cell_value_buffer;
          };
 
          *coeff *= -1;
@@ -203,70 +235,69 @@ struct Matrix matrix_scalar_mult(const struct Matrix A)
  }
 
 
-  формируется диагональный определитель, вычисляется произведение чисел главной диагонали,
-     умножается на коэффициент преобразования 
+    //преобразование к диагональному виду, вычисление определителя (перемножение элементов гл. диаг. и умножение на коэффициент преобразования) 
  double matrix_det(const struct Matrix A)
  {
      if (A.cols != A.rows)
          return NAN;
 
-     struct Matrix C = matrix_allocate(A.cols, A.rows);
-     if (C.data == NULL)
+     struct Matrix D = matrix_init(A.cols, A.rows);
+     if (D.data == NULL)
          return NAN;
-     matrix_copy(C, A);
+     matrix_copy(D, A);
 
      if (A.cols == 1) {
-         matrix_free(&C);
+         matrix_free(&D);
          return A.data[0];
      };
 
-     if (matrix_det_if_zero(C) == 0) {
-         matrix_free(&C);
+     if (matrix_det_if_zero(D) == 0) {
+         matrix_free(&D);
          return 0.0;
      };
 
      double coeff = 1.0;
      double diagonal = 1.0;
-     double buff_one, buff_two;
+     double row_number_buffer, cell_value_buffer;
 
      for (size_t diag = 0; diag < A.rows - 1; ++diag) {
-         matrix_det_prep(C, diag, &coeff);
-         coeff *= C.data[diag * A.cols + diag];
-         buff_one = C.data[diag * A.cols + diag];
+         matrix_det_prep(D, diag, &coeff);
+         coeff *= D.data[diag * A.cols + diag];
+         row_number_buffer = D.data[diag * A.cols + diag];
 
          for (size_t col = diag; col < A.cols; ++col)
-             C.data[diag * A.cols + col] /= buff_one;
+             D.data[diag * A.cols + col] /= row_number_buffer;
 
          for (size_t row = diag + 1; row < A.rows; ++row) {
-             buff_two = C.data[row * A.cols + diag];
+             cell_value_buffer = D.data[row * A.cols + diag];
              for (size_t col = diag; col < A.cols; ++col) {
-                 C.data[row * A.cols + col] = C.data[row * A.cols + col] - C.data[diag * A.cols + col] * buff_two;
+                 D.data[row * A.cols + col] = D.data[row * A.cols + col] - D.data[diag * A.cols + col] * cell_value_buffer;
              };
          };
      };
 
      for (size_t diag = 0; diag < A.rows; ++diag)
-         diagonal *= C.data[diag * A.cols + diag];
+         diagonal *= D.data[diag * A.cols + diag];
 
-     matrix_free(&C);
+     matrix_free(&D);
 
      double result = coeff * diagonal;
 
      return result;
- }*/
+} 
 
 
 int main()
 {
-    struct Matrix A, B, C, S, M, T; // +E (exp matrix) and +DET (not matrix)
+    struct Matrix A, B, C, S, M, T;  // +E (exp matrix) and +DET (not matrix)
 
     A = matrix_create(2, 2, (double[]) { 1., 2., 3., 4. });
     B = matrix_create(2, 2, (double[]) { 1., 0., 0., 1. });
 
     matrix_print(A);
     matrix_print(B);
-   // matrix_add(&B, &A);
-    
+  
+    // matrix_add(&B, &A);
 
     C = matrix_sum(A, B);
     matrix_print(C);
@@ -280,6 +311,12 @@ int main()
     T = matrix_tr(A, B);
     matrix_print(T);
 
+    double det;  // 
+    det = matrix_det(A);
+    printf("det A = %lf \n", det);
+    //e = matrix_det(E);
+    //printf("det E = %lf \n", e);
+
     matrix_free(&T);
     matrix_free(&M);
     matrix_free(&S);
@@ -288,45 +325,3 @@ int main()
     matrix_free(&B);
     return 0;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*ptr = malloc(30 * size of(double))
-for () {
-	*ptr = malloc(50 * size of(double));
-}
-(50 * 30)* size of(double) + 30 * size of(double*);
-
-
-enum  // ???
-memset(RES->data, 0.0, );  // как заполнить матрицу нулями
-
-Посмотри, как можно указатели обозначать:*/
-
-
-/*struct Error {
-	int level;
-	int char msg[100];
-};
-}
-struct Error malloc_error = { critical," " };
-
-Error_msg(malloc_error);*/
-
-
-
