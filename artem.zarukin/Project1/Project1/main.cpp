062e6be0626650a34a23d57765bc5c2c591ace36
Артём Зарукин <zarukin.aa@edu.spbstu.ru>
2023-11-30 06:13:38 +0300

    xdxd f

--- artem.zarukin/Project1/Project1/main.cpp
+++ artem.zarukin/Project1/Project1/main.cpp
@@ -1,33 +1,36 @@
 #include <stdio.h>
 #include <math.h>
 
-const int BOBWAGE = 20000000;
-const int ALICEWAGE = 20000000;
-const double MORTGAGEINTEREST = 0.07;
+const int BOBWAGE = 200 * 1000 * 100;
+const int ALICEWAGE = 200 * 1000 * 100;
+const int MORTGAGEINTEREST = 0.07;
 const double DEPOSITINTEREST = 0.009342;
-const int BOBINITIALCAPITAL = 100000000;
-const int ALICEINITIALCAPITAL = 100000000;
-const int DOWNPAYMENT = 10000000;
-const int MORTGAGEPERIOD = 30;
-const int ALICERENT = 3000000;
-const int BOBRENT = 1000000;
-const int BOBEXPENSES = 5000000;
-const int ALICEEXPENSES = 5000000;
-const long int APARTMENTPRICE = 2000000000;
+const int BOBINITIALCAPITAL = 1000 * 1000 * 100;
+const int ALICEINITIALCAPITAL = 1000 * 1000 * 100;
+const int DOWNPAYMENT = 1000 * 1000 * 100;
+const short int MORTGAGEPERIOD = 30;
+const int ALICERENT = 30 * 1000 * 100;
+const int BOBRENT = 10 * 1000 * 100;
+const int BOBEXPENSES = 50 * 1000 * 100;
+const int ALICEEXPENSES = 50 * 1000 * 100;
+const long int APARTMENTPRICE = 20 * 1000 * 1000 * 100;
 const double MONTHINFLATION = 0.005945;
 
-int totalPeriod = 30 * 12;
-int currentMonth = 1;
-double bobCapital = 100000000;
-double aliceCapital = 100000000;
+short int totalPeriod = 30 * 12;
+short int currentMonth = 1;
+double bobCapital = 100 * 1000 * 100;
+double aliceCapital = 1000 * 1000 * 100;
 double mortgageDebt = APARTMENTPRICE;
 double realApartmentPrice = APARTMENTPRICE;
 
-int main() {
-
+void InitialCapital()
+{
 	bobCapital = BOBINITIALCAPITAL - DOWNPAYMENT;
 	aliceCapital = ALICEINITIALCAPITAL;
+};
 
+void calc()
+{
 	for (currentMonth; currentMonth <= totalPeriod; currentMonth++) {
 
 		bobCapital = bobCapital + BOBWAGE;
@@ -45,22 +48,28 @@ int main() {
 		printf("\nThe end of month: ");
 		printf("%d", currentMonth);
 		printf("\nBob's estimater capital is ");
-		printf("%f", bobCapital);
+		printf("%f", bobCapital / 100);
 		printf(" rubles");
 		printf("\nAlice's estimater capital is ");
-		printf("%f", aliceCapital);
+		printf("%f", aliceCapital / 100);
 		printf("\n rubles");
 	}
 	printf("\n                   ");
 	printf("\n___________________");
 	printf("\n                   ");
 	printf("\nBob's net worth is ");
-	printf("%f", ((bobCapital + realApartmentPrice) / 100));
+	printf("%f", (bobCapital + realApartmentPrice) / 100);
 	printf("\n rubles");
 	printf("\nAlice's net worth is ");
-	printf("%f", (aliceCapital) / 100);
+	printf("%f", aliceCapital / 100);
 	printf("\n rubles");
+};
+
+int main() 
+{
+	InitialCapital();
+
+	calc();
 
 	return 0;
-
 }
