a1a98a6ea9b497edc6aa55540d601ce32bce16c4
Ксения Пономаренко <ponomarenko.ka@edu.spbstu.ru>
2023-11-30 09:41:21 +0300

    Matrix_upd4

--- Ksenia.Ponomarenko/Matrix/Matrix/Matrix.cpp
+++ Ksenia.Ponomarenko/Matrix/Matrix/Matrix.cpp
@@ -43,7 +43,7 @@ void matrix_delete(Matrix* A)
 void matrix_set_zero(Matrix A)
 {
     if (A.data == NULL) return;
-    memset(A.data, 0, sizeof(MatrixItem) * A.rows * A.cols);// Функция memset возвращает указатель на заполняемый массив. В нашем случае это массив из указателей A.data. ЧТО ЭТО БЛЯТЬ ТАКОЕ ЭТОТ ВАШ МЕМСЕТ.БЛЯТЬ
+    memset(A.data, 0, sizeof(MatrixItem) * A.rows * A.cols);
 }
 
 
@@ -55,22 +55,21 @@ void matrix_set_one(struct Matrix A)
 }
 
 
-void matrix_print(const struct Matrix A, const char* text) // Как понять, когда писать войд, а когда не войд? Функция вывода в матрице.
+void matrix_print(const struct Matrix A, const char* text) 
 {
-    printf("%s\n", text);// Какой нахуй текст? Откуда они его вщяли? Где он записан? А, это вообще указатель только на какую-то ячеку памяти, в которую будет записан непонятный текст. Заебись
-    for (size_t row = 0; row < A.rows; ++row) { // Col? Почему они считают, что можно просто так взять и с нихуя догбавить переменную? Это столбец. 1 шт.Все еще не поняла, что такое size_t
-        //Т.е. начиная по столбцам (хотя изначально в структуре было cols, а не col начинаем заполнять массив пока не достигнется количества стоблцов, которые соответстуют количетсву указателец стоблцов
+    printf("%s\n", text);
+    for (size_t row = 0; row < A.rows; ++row) { 
         printf("[ ");
         for (size_t col = 0; col < A.cols; ++col) { 
-            printf("%4.2f ", A.data[A.cols * row + col]);// Знак какой-то новый. Печатает внутрь указателей на дату что-то. Не поняла, почему внутри в кв скобочках [A.cols * row + col]
+            printf("%4.2f ", A.data[A.cols * row + col]);
         }
-        printf("]\n");// Закрыли типа один элемент скобошкой
+        printf("]\n");
     }
-    printf("\n"); //Перенос на др строку
+    printf("\n"); 
 }
 
 
-void value_print(MatrixItem A, const char* text) // Передаем что? Значение a? Или указатель на значения А? Все еще не понятно, где text
+void value_print(MatrixItem A, const char* text) 
 {
     printf("%s\n", text);
 
@@ -79,28 +78,25 @@ void value_print(MatrixItem A, const char* text) // Передаем что? Значение a? Ил
 
 
 // A += B
-void matrix_add( Matrix A, Matrix B) // Функция добавления одинаковой матрицы к первой другой матрице несколько раз? Чем эта функция отличается от следующей?
+void matrix_add( Matrix A, Matrix B) 
 {
-    if (A.cols != B.cols || A.rows != B.rows) return; // Почему return 1? А не 0? Потому что 0 занят ниже? И надо возвращать что-то другое. И это ошибка, ибо нельзя суммировать разнокалиберные матрицы. 
-    // Хотя ошибка скорее в следующей функции, а тут не понятно что
+    if (A.cols != B.cols || A.rows != B.rows) return; 
 
-    for (size_t idx = 0; idx < A.cols * A.rows; ++idx) // Jт куска структуры, под номером 0 (idx=0),до куска структуры меньше умножения столбцов и строк (указателей!) и инкрементировать.
-        // Типа мы просто не говорим, пл толбцам или по строкам заполряем, а просто по номерам? А как эти номера идут? Где это написано?
-        A.data[idx] += B.data[idx]; // Присваивать каждому элементу увеличенное на значение числа в другом таком же элементе 
+    for (size_t idx = 0; idx < A.cols * A.rows; ++idx) 
+        A.data[idx] += B.data[idx]; 
 }
 
 
 // C = A + B
- Matrix matrix_sum( Matrix A, Matrix B) // Суммирование двух матриц. Ну тут похоже точно нельзя складывать разнокалиберные
+ Matrix matrix_sum( Matrix A, Matrix B) 
 {
-    if (A.cols != B.cols || A.rows != B.rows) return NULL_MATRIX; // Почему здесь нулевая, а в предыдущей функции возвращалось 1?
+    if (A.cols != B.cols || A.rows != B.rows) return NULL_MATRIX; 
 
-    Matrix C = matrix_allocation(A.cols, A.rows); // Создаем переменную типа структуры Matrix, называем ее sum. Выделяем под нее ячейки памяти через alloc в количестве A.cols /rows
-    if (C.data == NULL) return NULL_MATRIX; // Если суммарная матрица выдает пустые указатели (почему-то???) возвращаем нулевую матрицу из 16 строки?
-    // переписать ебаный костыль на нормальную релизацию сложения
-    memcpy(C.data, A.data, sizeof(MatrixItem) * A.cols * A.rows); // Копируем память от data (к какой именно матрице относится data? Почему не сказали?) Сами указатели на числа в матрице А
-    // и количество памяти, равное количествву памяти под все элементы
-    matrix_add(C, B); // Добавить C, где мы выделили память под 
+    Matrix C = matrix_allocation(A.cols, A.rows); 
+    if (C.data == NULL) return NULL_MATRIX; 
+    
+    memcpy(C.data, A.data, sizeof(MatrixItem) * A.cols * A.rows); 
+    matrix_add(C, B);  
 
     return C;
 }
@@ -124,8 +120,8 @@ Matrix matrix_diff( Matrix A,  Matrix B)
  Matrix D = matrix_allocation(A.cols, A.rows);
     if (D.data == NULL) return NULL_MATRIX;
 
-    memcpy(D.data, A.data, sizeof(MatrixItem) * A.cols * A.rows); // Здесь A.data - это сколько выделить памяти? Типа откуда скопировать ее объем?
-    matrix_add(D, B); // К чему прибавляем, что прибавляем
+    memcpy(D.data, A.data, sizeof(MatrixItem) * A.cols * A.rows); 
+    matrix_add(D, B); 
 
     return D;
 }
@@ -136,21 +132,17 @@ Matrix matrix_diff( Matrix A,  Matrix B)
 {
     if (A.cols != B.rows) return NULL_MATRIX;
 
- Matrix mult = matrix_allocation(A.rows, B.cols); //Типа потому что умножение, поэтому надо выделять строки как столбцы, а столбцы, как строки?
-    if (mult.data == NULL) return NULL_MATRIX; // Если ничего случайно не записалось, то возвращаем нулевую. Это ошибка так прописана
+ Matrix mult = matrix_allocation(A.rows, B.cols); 
+    if (mult.data == NULL) return NULL_MATRIX; 
 
-    for (size_t rowA = 0; rowA < A.rows; ++rowA) {      // Цикл для: вводим новый счетчик (потому что старый уже использовался? Или для понимания, что на что умножается
-        // Инкрементируем почему-то по строкам. Это принципиально? Типа есть ли разница в зависимости от функции, по чему суммируем внешний цикл
+    for (size_t rowA = 0; rowA < A.rows; ++rowA) {     
+        
         for (size_t colB = 0; colB < B.cols; ++colB) {
-            MatrixItem sum = 0;                         // Ввели новую локальную переменную double, которая зачем-то суммирует (или просто так называется sum?)
-            for (size_t idx = 0; idx < A.cols; ++idx) {     // 
-                sum += A.data[rowA * A.cols + idx] * B.data[idx * B.cols + colB];      // Эта изначально в 145 строчке обнуленная сумма прибавляет значение к себе каждвй раз. Это значение
-                // Обращается к ОПРЕДЕЛЕННОМУ ЭЛЕМЕНТУ, НЕ К АДРЕСУ БЛЯТЬ. А точнее к строке, которая была вычислена в счетчике выше, умноженной на количество столбцов +текущий номер элемента
-                //Не поняла математику. РАЗОБРАТЬСЯ
+            MatrixItem sum = 0;                         
+            for (size_t idx = 0; idx < A.cols; ++idx) {     
+                sum += A.data[rowA * A.cols + idx] * B.data[idx * B.cols + colB];     
             }
-            mult.data[rowA * mult.cols + colB] = sum; // Здесь описаны значения, которые получаются в результирующей матрице. Они берутся из номеров, которые получаются
-            // через умножение строчки, котрая в счетчике и почему-то mult.cols. Откуда там точка? Почему они так к столбцам обращаются? И плюс столбец матрицы B
-            //Не поняла математику. Вообще. Нихуя. И это все равно sum (ПОЧЕМУ-ТО)
+            mult.data[rowA * mult.cols + colB] = sum; 
         }
     }
     return mult;
@@ -158,17 +150,16 @@ Matrix matrix_diff( Matrix A,  Matrix B)
 
 
 // A *= k
-void matrix_mult_k(Matrix A, const MatrixItem k) //  Умножение матрицы на число в ту же матрицу
+void matrix_mult_k(Matrix A, const MatrixItem k) 
 {
-    for (size_t idx = 0; idx < A.cols * A.rows; ++idx) { // Перебирает индексы, которые номера, подряд, внутри одной матрицы. Почемц нет провкрки на долбаеба? Потому что усножаем на число?
-        A.data[idx] *= k; // 
+    for (size_t idx = 0; idx < A.cols * A.rows; ++idx) { 
     }
     return;
 }
 
 
 // A /= k
-void matrix_div_k(struct Matrix A, const MatrixItem k) //Деление матрицы на число в ту же матрицу
+void matrix_div_k(struct Matrix A, const MatrixItem k)
 {
     for (size_t idx = 0; idx < A.cols * A.rows; ++idx) {
         A.data[idx] /= k;
@@ -181,13 +172,11 @@ void matrix_div_k(struct Matrix A, const MatrixItem k) //Деление матрицы на числ
 struct Matrix matrix_transpose(struct Matrix A)
 {
     Matrix tpose = matrix_allocation(A.rows, A.cols);
-    if (tpose.data == NULL) return tpose; // Это он так при ошибке возвращает первую нетранспонированную матрицу? Странно, потому что кажется, что мог не нажать или еще что
+    if (tpose.data == NULL) return tpose; 
 
-    for (size_t row = 0; row < tpose.rows; row++) { // А где мы объявили это значение tpose.rows? Это строки из матрицы A, судя по 183 строчке?
+    for (size_t row = 0; row < tpose.rows; row++) { 
         for (size_t col = 0; col < tpose.cols; col++) {
-            tpose.data[col * tpose.cols + row] = A.data[row * A.cols + col]; // Заполняем массив транспонированной матрицы. Обращаемся к элементам под номером: столбец по счетчику,
-            // умноженный на  столбцы транспонироыанной матрицы (почему??) и плюс к этому умноженному номер строки из счетчика (той же самой, что и столбца).
-            //Математику не поняла
+            tpose.data[col * tpose.cols + row] = A.data[row * A.cols + col];
 
         }
     }
