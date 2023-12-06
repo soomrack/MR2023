257d48b6aceb4df66ea1c32a322fbb07954744f3
paralarnax <semkarim2018@gmail.com>
2023-12-06 16:05:16 +0300

    Second Task

---
+++ Каримуллин Семён/Matrix/build/CMakeFiles/main.dir/main.c.o.d
@@ -0,0 +1,3 @@
+CMakeFiles/main.dir/main.c.o: \
+ /home/prlx/MR2023/Каримуллин\ Семён/Matrix/main.c \
+ /usr/include/stdc-predef.h
---
+++ Каримуллин Семён/Matrix/build/CMakeFiles/matrix.dir/Matrix.c.o.d
@@ -0,0 +1,3 @@
+CMakeFiles/matrix.dir/Matrix.c.o: \
+ /home/prlx/MR2023/Каримуллин\ Семён/Matrix/Matrix.c \
+ /usr/include/stdc-predef.h
