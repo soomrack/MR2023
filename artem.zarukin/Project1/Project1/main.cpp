42c1eb0f1bc307f72cc9587be2c3c2319ec99167
Артём Зарукин <zarukin.aa@edu.spbstu.ru>
2023-12-14 10:10:56 +0300

    aqwr

--- artem.zarukin/Project1/Project1/main.cpp
+++ artem.zarukin/Project1/Project1/main.cpp
@@ -1,67 +1,84 @@
 #include <stdio.h>
 #include <math.h>
+#include <string.h>
 
-const int BOBWAGE = 200 * 1000 * 100;
-const int ALICEWAGE = 200 * 1000 * 100;
+struct Person
+{
+	int wage;
+	int initialCapital;
+	int rent;
+	int expenses;
+	double capital;
+	char name[6];
+};
+
+
+struct Person alice;
+struct Person bob;
+
 const int MORTGAGEINTEREST = 0.07;
 const double DEPOSITINTEREST = 0.009342;
-const int BOBINITIALCAPITAL = 1000 * 1000 * 100;
-const int ALICEINITIALCAPITAL = 1000 * 1000 * 100;
 const int DOWNPAYMENT = 1000 * 1000 * 100;
 const short int MORTGAGEPERIOD = 30;
-const int ALICERENT = 30 * 1000 * 100;
-const int BOBRENT = 10 * 1000 * 100;
-const int BOBEXPENSES = 50 * 1000 * 100;
-const int ALICEEXPENSES = 50 * 1000 * 100;
 const long int APARTMENTPRICE = 20 * 1000 * 1000 * 100;
 const double MONTHINFLATION = 0.005945;
 
 short int totalPeriod = 30 * 12;
 short int currentMonth = 1;
-double bobCapital = 100 * 1000 * 100;
-double aliceCapital = 1000 * 1000 * 100;
 double mortgageDebt = APARTMENTPRICE;
 double realApartmentPrice = APARTMENTPRICE;
 
 void InitialCapital()
 {
-	bobCapital = BOBINITIALCAPITAL - DOWNPAYMENT;
-	aliceCapital = ALICEINITIALCAPITAL;
+
+	bob.wage = 200 * 1000 * 100;
+	bob.initialCapital = 1000 * 1000 * 100;
+	bob.rent = 10 * 1000 * 100;
+	bob.expenses = 50 * 1000 * 100;
+
+	alice.wage = 200 * 1000 * 100;
+	alice.initialCapital = 1000 * 1000 * 100;
+	alice.rent = 30 * 1000 * 100;
+	alice.expenses = 50 * 1000 * 100;
+
+	bob.capital = bob.initialCapital - DOWNPAYMENT;
+	alice.capital = alice.initialCapital;
+
 };
 
 void calc()
 {
 	for (currentMonth; currentMonth <= totalPeriod; currentMonth++) {
 
-		bobCapital = bobCapital + BOBWAGE;
-		bobCapital = bobCapital - BOBEXPENSES - BOBEXPENSES * MONTHINFLATION * currentMonth;
-		bobCapital = bobCapital - APARTMENTPRICE / totalPeriod - mortgageDebt * MORTGAGEINTEREST / totalPeriod;
+		bob.capital = bob.capital + bob.wage;
+		bob.capital = bob.capital - bob.expenses - bob.expenses * MONTHINFLATION * currentMonth;
+		bob.capital = bob.capital - APARTMENTPRICE / totalPeriod - mortgageDebt * MORTGAGEINTEREST / totalPeriod;
 		mortgageDebt = mortgageDebt - APARTMENTPRICE / totalPeriod - mortgageDebt * MORTGAGEINTEREST / totalPeriod;
-		bobCapital = bobCapital - BOBRENT - BOBRENT * MONTHINFLATION * currentMonth;
+		bob.capital = bob.capital - bob.rent - bob.rent * MONTHINFLATION * currentMonth;
 		realApartmentPrice = realApartmentPrice + realApartmentPrice * MONTHINFLATION;
 
-		aliceCapital = aliceCapital + ALICEWAGE + ALICEWAGE * MONTHINFLATION * currentMonth;
-		aliceCapital = aliceCapital - ALICEEXPENSES - ALICEEXPENSES * MONTHINFLATION * currentMonth;
-		aliceCapital = aliceCapital - ALICERENT - ALICERENT * MONTHINFLATION * currentMonth;
-		aliceCapital = aliceCapital + aliceCapital * DEPOSITINTEREST;
+		alice.capital = alice.capital + alice.wage + alice.wage * MONTHINFLATION * currentMonth;
+		alice.capital = alice.capital - alice.expenses - alice.expenses * MONTHINFLATION * currentMonth;
+		alice.capital = alice.capital - alice.rent - alice.rent * MONTHINFLATION * currentMonth;
+		alice.capital = alice.capital + alice.capital * DEPOSITINTEREST;
 
 		printf("\nThe end of month: ");
 		printf("%d", currentMonth);
 		printf("\nBob's estimater capital is ");
-		printf("%f", bobCapital / 100);
+		printf("%f", bob.capital / 100);
 		printf(" rubles");
 		printf("\nAlice's estimater capital is ");
-		printf("%f", aliceCapital / 100);
+		printf("%f", alice.capital / 100);
 		printf("\n rubles");
 	}
 	printf("\n                   ");
 	printf("\n___________________");
 	printf("\n                   ");
 	printf("\nBob's net worth is ");
-	printf("%f", (bobCapital + realApartmentPrice) / 100);
+	printf("%f", (bob.capital + realApartmentPrice) / 100);
 	printf("\n rubles");
 	printf("\nAlice's net worth is ");
-	printf("%f", aliceCapital / 100);
+	printf("%f", alice.capital / 100);
 	printf("\n rubles");
 };
 
