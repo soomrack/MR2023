445e96453a70518b2060636b3df6dbcec1ec2c0e
denispelm <144234708+denispelm@users.noreply.github.com>
2024-02-17 16:40:18 +0300

    Dust to dust

--- pelmenev.denis.prog/dptask_array_sort.cpp
+++ pelmenev.denis.prog/dptask_array_sort.cpp
@@ -32,18 +32,22 @@ void sort_bubble(std::vector <int> &array)
 
 void merge(std::vector <int> &array, std::vector <int> &sub_array_left, std::vector <int> &sub_array_right)
 {
-    for (size_t idx = 0; idx < sub_array_right.size(); ++idx) {
+    for (size_t idx = 0; idx < array.size(); ++idx) {
+        if (sub_array_left.empty() && sub_array_right.empty()) break;
+
         if (sub_array_left.empty() && !sub_array_right.empty()) {
             array[idx] = sub_array_right[0];
             sub_array_right.erase(sub_array_right.begin());
+            break;
         }
         
         if (!sub_array_left.empty() && sub_array_right.empty()) {
             array[idx] = sub_array_left[0];
             sub_array_left.erase(sub_array_left.begin());
+            break;
         }
 
-        if (sub_array_left[0] <= sub_array_right[0]) {
+        if (sub_array_left[0] < sub_array_right[0]) {
             array[idx] = sub_array_left[0];
             sub_array_left.erase(sub_array_left.begin());
         }
@@ -52,39 +56,48 @@ void merge(std::vector <int> &array, std::vector <int> &sub_array_left, std::vec
             sub_array_right.erase(sub_array_right.begin());
         }
     }
-
-    sub_array_left.clear();
-    sub_array_right.clear();
 }
 
 
 void sort_merge(std::vector <int> &array)
 {
-    if (array.begin() != array.end()) {
-        size_t ar_end_left = (size_t)(std::ceil(array.size() / 2));
+    if (array.size() > 1) {
+        size_t ar_end_left = (size_t)(std::round(array.size() / 2));
         size_t ar_end_right = array.size();
+
+        //std::cout << "Length: " << ar_end_left << " " << ar_end_right << std::endl;     //DEBUG
 
         std::vector <int> sub_array_left;
         for (size_t idx = 0; idx < ar_end_left; ++idx)
             sub_array_left.push_back(array[idx]);
+        
+        //std::cout << "sub_array_left:  ";
+        //print_array(sub_array_left);
 
         std::vector <int> sub_array_right;
         for (size_t idx = ar_end_left; idx < ar_end_right; ++idx)
             sub_array_right.push_back(array[idx]);
+        
+        //std::cout << "sub_array_right:  ";
+        //print_array(sub_array_right);
 
         sort_merge(sub_array_left);
         sort_merge(sub_array_right);
 
         merge(array, sub_array_left, sub_array_right);
+
+        //std::cout << "Merged array:  ";
+        //print_array(array);
     }
 }
 
 
 int main()
 {
-    std::vector <int> a = {5, 4, 1, 3, 3, 8};
+    std::vector <int> a = {5, 4, 1, 3, 7, 3};
     print_array(a);
     std::cout << "--------------" << std::endl;
+    //sort_bubble(a);
     sort_merge(a);
     print_array(a);
     
