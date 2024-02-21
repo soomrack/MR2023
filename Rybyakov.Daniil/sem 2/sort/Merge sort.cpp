c5da582b53d7e0e5bffdc0c8cce2a0be832793a0
rybyakov19 <144235855+rybyakov19@users.noreply.github.com>
2024-02-21 18:15:18 +0300

    1

--- Rybyakov.Daniil/sem 2/sort/Merge sort.cpp
+++ Rybyakov.Daniil/sem 2/sort/Merge sort.cpp
@@ -2,45 +2,50 @@
 using namespace std;
 
 
-void merge(int* array, int begin, int end)
+void merge(int* array, int begin, int end, int* help_array)
 {
-	int i = begin;
+	int idx_1 = begin;
 	int middle = begin + (end - begin) / 2;
-	int j = middle + 1;
-	int k = 0;
-	int help_array[1000];
+	int idx_2 = middle + 1;
+	int idx_3 = 0;
 
-	for (; i <= middle && j <= end; k++) {
-		if (array[i] <= array[j]) { help_array[k] = array[i]; i++; }
-		else { help_array[k] = array[j]; j++; }
+	for (; idx_1 <= middle && idx_2 <= end; idx_3++) {
+		if (array[idx_1] <= array[idx_2]) { help_array[idx_3] = array[idx_1]; idx_1++; }
+		else { help_array[idx_3] = array[idx_2]; idx_2++; }
 	}
-	for (; i <= middle;) {
-		help_array[k] = array[i]; i++; k++;
+	for (; idx_1 <= middle; idx_1++, idx_3++) {
+		help_array[idx_3] = array[idx_1];
 	}
-	for (; j <= end;) {
-		help_array[k] = array[j]; j++; k++;
+	for (; idx_2 <= end; idx_2++, idx_3++) {
+		help_array[idx_3] = array[idx_2];
 	}
-	for (i = 0; i < k; i++) {
-		array[begin + i] = help_array[i];
+	for (idx_1 = 0; idx_1 < idx_3; idx_1++) {
+		array[begin + idx_1] = help_array[idx_1];
 	}
 }
 
 
-void merge_sort(int* array, int left, int right)
+void swap_func(int* array, int index)
+{
+	int swap = array[index];
+	array[index] = array[index + 1];
+	array[index + 1] = swap;
+}
+
+
+void merge_sort(int* array, int left, int right, int* help_array)
 {
 	int swap = 0;
 	if (left < right) {
 		if (right - left == 1) {
 			if (array[left] > array[right]) {
-				swap = array[left];
-				array[left] = array[right];
-				array[right] = swap;
+				swap_func(array, left);
 			}
 		}
 		else {
-			merge_sort(array, left, left + (right - left) / 2);
-			merge_sort(array, left + (right - left) / 2 + 1, right);
-			merge(array, left, right);
+			merge_sort(array, left, left + (right - left) / 2, help_array);
+			merge_sort(array, left + (right - left) / 2 + 1, right, help_array);
+			merge(array, left, right, help_array);
 		}
 	}
 }
@@ -58,9 +63,15 @@ void array_print(int array_size, int *array)
 int main()
 {
 	int array_size = 10;
-	int *array = new int[10]{ 2, 4, 1, 11, 9, 7, 4, 9, 55, 23 };
+	int *array = new int[10]{ 2, 4, 1, 11, 9, 7, 4, 9, 50, 20 };
+
+	int help_array_size = 10;
+	int* help_array = new int[10];
+		
 	array_print(array_size, array);
-	merge_sort(array, 0, array_size - 1);
+	merge_sort(array, 0, array_size - 1, help_array);
 	array_print(array_size, array);
+
 	delete[] array;
+	delete[] help_array;
 }
