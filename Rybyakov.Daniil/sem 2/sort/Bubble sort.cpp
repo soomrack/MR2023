0964b47343aec6f6486a592c14d55b416d46abf4
rybyakov19 <144235855+rybyakov19@users.noreply.github.com>
2024-02-21 17:46:39 +0300

    1

--- Rybyakov.Daniil/sem 2/sort/Bubble sort.cpp
+++ Rybyakov.Daniil/sem 2/sort/Bubble sort.cpp
@@ -3,21 +3,27 @@ using namespace std;
 
 void print_array(int* array, int array_size)
 {
-	for (int i = 0; i < array_size; ++i) {
-		cout << "  " << array[i];
+	for (int array_elem = 0; array_elem < array_size; ++array_elem) {
+		cout << "  " << array[array_elem];
 	}
 	cout << "\n";
 }
 
 
+void swap_func(int *array, int index)
+{
+	int swap = array[index];
+	array[index] = array[index + 1];
+	array[index + 1] = swap;
+}
+
+
 void bubble_sort(int *array, int array_size) 
 {
-	for (int k = 0; k < array_size; ++k) {
-		for (int i = 0; i < array_size - k - 1; ++i) {
-			if (array[i] > array[i + 1]) {
-				int swap = array[i];
-				array[i] = array[i + 1];
-				array[i + 1] = swap;
+	for (int index_1 = 0; index_1 < array_size; ++index_1) {
+		for (int index_2 = 0; index_2 < array_size - index_1 - 1; ++index_2) {
+			if (array[index_2] > array[index_2 + 1]) {
+				swap_func(array, index_2);
 			}
 		}
 	}
@@ -28,7 +34,7 @@ void bubble_sort(int *array, int array_size)
 int main() 
 {
 	int array_size = 10;
-	int* array = new int[10] { 2, 4, 1, 11, 9, 7, 4, 9, 55, 23 };
+	int* array = new int[10] { 2, 4, 1, 11, 9, 7, 4, 9, 50, 20 };
 	print_array(array, array_size);
 	bubble_sort(array, array_size);
 	
