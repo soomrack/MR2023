9e16da6bca9a76099772a62e0a9cf34cd147cdf9
Networksx333 <127897894+Networksx333@users.noreply.github.com>
2024-02-23 20:42:52 +0300

    fixes + 2 new sorting functions
    - Removed unintuitive names where they exist
    - Now the creation of a temporary array for merge sort occurs only once
    - Added insertion sort function
    - Added heap sort function

--- Sudneko Vladislav/sorting_algorithms.cpp
+++ Sudneko Vladislav/sorting_algorithms.cpp
@@ -1,15 +1,15 @@
 #include <iostream>
 #include <vector>
+#include <bits/stdc++.h>
 
 using namespace std;
-
 
 /**
  * Prints the elements of a vector.
  *
  * @param vector The vector containing the elements to be printed.
  */
-void print_ar(const auto& vector) {
+void print_array(const auto& vector) {
     cout << "{ ";
     for (auto element : vector) {
         cout << element << " ";
@@ -23,9 +23,9 @@ void print_ar(const auto& vector) {
  *
  * @param array The array to be sorted.
  */
-void bubble_sort(auto& array) {
+void bubble_sort(auto& array, int array_size) {
     auto temp = array[0];
-    size_t unsorted = array.size();
+    size_t unsorted = array_size;
     for (size_t iteration = 0; iteration < unsorted; iteration++, unsorted--) {
         for (size_t element = 0; element < unsorted - 1; element++) {
             if (array[element] > array[element+1]) {
@@ -45,15 +45,14 @@ void bubble_sort(auto& array) {
  * @param start_index The starting index of the portion to be sorted.
  * @param end_index The ending index of the portion to be sorted.
  * @param middle_index The middle index dividing the two sorted halves.
- * @param array_length The length of the array.
+ * @param array_size The length of the array.
  */
-void merge(auto& array, size_t start_index, size_t end_index, size_t middle_index, size_t array_length)
+void merge(auto& array, auto& temp, size_t start_index, size_t end_index, size_t middle_index, size_t array_size)
 {
-    int left, right, temp_index, temp[array_length];
+    size_t left, right, temp_index;
     left = start_index;
     temp_index = start_index;
     right = middle_index + 1;
-    int t = 0;
     while (left <= middle_index && right <= end_index)
     {
         if (array[left] < array[right])
@@ -89,36 +88,139 @@ void merge(auto& array, size_t start_index, size_t end_index, size_t middle_inde
 
 
 /**
- * Sorts a vector in ascending order using the merge sort algorithm.
+ * The main function for merge sort, contains the main stages of the algorithm.
  *
- * @param a The array to be sorted.
- * @param start_index The starting index of the vector to be sorted.
- * @param end_index The ending index of the vector to be sorted.
+ * @param array         The array to be sorted.
+ * @param temp          Temporary array to store values
+ * @param start_index   The starting index of the vector to be sorted.
+ * @param end_index     The ending index of the vector to be sorted.
  */
-void merge_sort(auto& a, size_t start_index, size_t end_index) {
+void merge_sort_main(auto& array, auto temp, size_t start_index, size_t end_index) {
     if (start_index < end_index) {
         size_t middle_index = (start_index + end_index) / 2;
-        merge_sort(a, start_index, middle_index);
-        merge_sort(a, middle_index+1, end_index);
+        merge_sort_main(array, temp, start_index, middle_index);
+        merge_sort_main(array, temp, middle_index+1, end_index);
 
-        merge(a, start_index, end_index, middle_index, a.size());
+        merge(array, temp, start_index, end_index, middle_index, end_index+1);
     }
     return;
 }
 
 
+/**
+ * The function creates a temporary array to store elements and calls 
+ * the main merge sort function.
+ *
+ * @param array         The array to be sorted.
+ * @param start_index   The starting index of the vector to be sorted.
+ * @param end_index     The ending index of the vector to be sorted.
+ */
+void merge_sort(auto& array, size_t start_index, size_t end_index) {
+    using temp_type = remove_reference<decltype( array[0] )>::type;
+    int temp_size = end_index+1; 
+    temp_type temp[temp_size];
+    merge_sort_main(array, temp, start_index, end_index);
+}
+
+
+/**
+ * A function that sorts a graph.
+ * 
+ * @param array             The array to be heapified.
+ * @param array_size        The size of the array.
+ * @param non_leaf_node     The index to start heapifying from.
+ */
+void heapify(auto& array, int array_size, int non_leaf_node) { 
+    int largest = non_leaf_node; 
+    int l = 2*non_leaf_node + 1; 
+    int r = 2*non_leaf_node + 2; 
+    if (l < array_size && array[l] > array[largest]) { 
+        largest = l; 
+    } 
+    if (r < array_size && array[r] > array[largest]) { 
+        largest = r; 
+    } 
+    if (largest != non_leaf_node) { 
+        swap(array[largest], array[non_leaf_node]); 
+        heapify(array, array_size, largest); 
+    } 
+} 
+
+
+/**
+ * Function to perform heap sort on the given array.
+ * 
+ * @param array          The array to be sorted.
+ * @param array_size     The size of the array.
+ */
+void heap_sort(auto& array, int array_size) { 
+    for (int non_leaf_node = floor(array_size/2) - 1; non_leaf_node >= 0; non_leaf_node--) { 
+        heapify(array, array_size, non_leaf_node); 
+    } 
+    for (int non_leaf_node = array_size - 1; non_leaf_node >= 0; non_leaf_node--) {
+        swap(array[0], array[non_leaf_node]);
+        heapify(array, non_leaf_node, 0); 
+    } 
+}
+
+
+/**
+ * Function to perform insertion sort on the given array.
+ * 
+ * @param array          The array to be sorted.
+ * @param array_size     The size of the array.
+ */
+void insertion_sort(auto& array, int array_size) {
+    int temp;
+    int index_of_element_before;
+    for (int index_of_current_element = 1; index_of_current_element < array_size; index_of_current_element++) {
+        temp =  array[index_of_current_element];
+        index_of_element_before = index_of_current_element - 1;
+
+        while ((index_of_element_before >= 0) && (array[index_of_element_before] > temp)) {
+            array[index_of_element_before + 1] = array[index_of_element_before];
+            index_of_element_before--;
+        }
+        array[index_of_element_before+1] = temp;
+    }
+}
+
+
 int main() {
-    vector<int> a = {4, 8, 1, 5, 9, 0, 2, 7, 3, 6};
+    vector<int> array = {4, 8, 1, 5, 9, 0, 2, 7, 3, 6};
 
-    bubble_sort(a);
-    print_ar(a);
+    // Bubble sort
+    // ***********************************
+    bubble_sort(array, array.size());
+    cout << "Bubble sort:" << endl;
+    print_array(array);
+    // ***********************************
 
-    a = {4, 8, 1, 5, 9, 0, 2, 7, 3, 6};
-    size_t start_index = 0;
-    size_t end_index = a.size() - 1;
+    // Merge sort
+    // ***********************************
+    array = {4, 8, 1, 5, 9, 0, 2, 7, 3, 6};
+    int start_index = 0;
+    int end_index = array.size() - 1;
+    merge_sort(array, start_index, end_index);
+    cout << "Merge sort:" << endl;
+    print_array(array);
+    // ***********************************
 
-    merge_sort(a, start_index, end_index);
-    print_ar(a);
+    // Heap sort
+    // ***********************************
+    array = {4, 8, 1, 5, 9, 0, 2, 7, 3, 6};
+    heap_sort(array, array.size());
+    cout << "Heap sort:" << endl;
+    print_array(array);
+    // ***********************************
+
+    // Insertion sort
+    // ***********************************
+    array = {4, 8, 1, 5, 9, 0, 2, 7, 3, 6};
+    cout << "Insertion sort:" << endl;
+    insertion_sort(array, array.size());
+    print_array(array);
+    // ***********************************
 
     return 0;
 }
