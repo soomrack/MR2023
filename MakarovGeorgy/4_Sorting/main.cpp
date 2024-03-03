e3d8799d485f4af00fa921754d1bac1af5146d26
Georgy Makarov <makarov@paradoxical.ru>
2024-03-03 23:22:02 +0300

    Sorting fix

--- MakarovGeorgy/4_Sorting/main.cpp
+++ MakarovGeorgy/4_Sorting/main.cpp
@@ -12,62 +12,40 @@ void print(const std::string &info, const std::vector<int> &vector) {
 // Bubble sort
 
 void bubble_sort(std::vector<int> &vector) {
-    const size_t size = vector.size();
-
-    for (size_t idx = 0; idx < size; idx++)
-        for (size_t item = 0; item < size - 1; item++)
+    for (size_t sorted = 0; sorted < vector.size(); sorted++)
+        for (size_t item = 0; item < vector.size() - 1; item++)
             if (vector[item] > vector[item + 1]) std::swap(vector[item], vector[item + 1]);
 }
 
 
 // Merge sort
 
-void merge(std::vector<int> &vector, size_t start, size_t middle, size_t end) {
-    const size_t size_first = middle + 1 - start;
-    const size_t size_second = end - middle;
+void merge(std::vector<int> &vector, std::vector<int> &left, std::vector<int> &right) {
+    size_t left_idx = 0;
+    size_t right_idx = 0;
 
-    std::vector<int> vector_first(size_first);
-    std::vector<int> vector_second(size_second);
-
-    for (size_t i = 0; i < size_first; i++) vector_first[i] = vector[start + i];
-    for (size_t i = 0; i < size_second; i++) vector_second[i] = vector[middle + 1 + i];
-
-    int idx = 0;
-    int jdx = 0;
-    int kdx = (int) start;
-
-    while (idx < size_first && jdx < size_second) {
-        if (vector_first[idx] <= vector_second[jdx]) {
-            vector[kdx] = vector_first[idx];
-            idx++;
-        } else {
-            vector[kdx] = vector_second[jdx];
-            jdx++;
-        }
-        kdx++;
+    while (left_idx < left.size() && right_idx < right.size()) {
+        if (left.at(left_idx) < right.at(right_idx)) vector.push_back(left.at(left_idx++));
+        else vector.push_back(right.at(right_idx++));
     }
 
-    while (idx < size_first) {
-        vector[kdx] = vector_first[idx];
-        idx++;
-        kdx++;
-    }
-}
-
-void merge_sort_body(std::vector<int> &vector, size_t start, size_t end) {
-    size_t middle = start + (end - start) / 2;
-
-    merge_sort_body(vector, start, middle);
-    merge_sort_body(vector, middle + 1, end);
-
-    merge(vector, start, middle, end);
+    while (left_idx < left.size()) vector.push_back(left.at(left_idx++));
+    while (right_idx < right.size()) vector.push_back(right.at(right_idx++));
 }
 
 void merge_sort(std::vector<int> &vector) {
-    const size_t start = 0;
-    const size_t end = vector.size() - 1;
+    if (vector.size() <= 1) return;
 
-    merge_sort_body(vector, start, end);
+    const auto middle = vector.begin() + vector.size() / 2;
+
+    std::vector<int> left(vector.begin(), middle);
+    std::vector<int> right(middle, vector.end());
+
+    merge_sort(left);
+    merge_sort(right);
+
+    vector.clear();
+    merge(vector, left, right);
 }
 
 
@@ -92,13 +70,11 @@ void heapify(std::vector<int> &vector, size_t size, int idx) {
 }
 
 void heap_sort(std::vector<int> &vector) {
-    const size_t size = vector.size();
-
-    for (int idx = (int) size / 2 - 1; idx >= 0; idx--) {
-        heapify(vector, size, idx);
+    for (int idx = (int) vector.size() / 2 - 1; idx >= 0; idx--) {
+        heapify(vector, vector.size(), idx);
     }
 
-    for (int idx = (int) size - 1; idx > 0; idx--) {
+    for (int idx = (int) vector.size() - 1; idx > 0; idx--) {
         std::swap(vector[0], vector[idx]);
         heapify(vector, idx, 0);
     }
@@ -108,21 +84,16 @@ void heap_sort(std::vector<int> &vector) {
 // Insertion sort
 
 void insertion_sort(std::vector<int> &vector) {
-    const size_t size = vector.size();
+    for (int sorted = 1; sorted < vector.size(); sorted++) {
+        int item = vector[sorted];
 
-    int idx_old;
-    int temp;
-
-    for (int idx = 1; idx < size; idx++) {
-        temp = vector[idx];
-        idx_old = idx - 1;
-
-        while ((idx_old >= 0) && (vector[idx_old] > temp)) {
-            vector[idx_old + 1] = vector[idx_old];
-            idx_old--;
+        int idx = sorted - 1;  // integer as it can be negative
+        while ((idx >= 0) && (vector[idx] > item)) {
+            vector[idx + 1] = vector[idx];
+            idx--;
         }
 
-        vector[idx_old + 1] = temp;
+        vector[idx + 1] = item;
     }
 }
 
