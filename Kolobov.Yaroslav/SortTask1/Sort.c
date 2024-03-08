40f0aa6e9f41d5e8454285b6472872ba84d4e19c
DolcheAlbero <kolobokyar@gmail.com>
2024-03-08 12:44:22 +0300

    merge sort fix and insertion sort

--- Kolobov.Yaroslav/SortTask1/Sort.c
+++ Kolobov.Yaroslav/SortTask1/Sort.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void mass_print(int array[], const int number_of_elemnts)
+void mass_print(int array[], const int number_of_elements)
 {
-    for (int printed_elements = 0; printed_elements < number_of_elemnts; printed_elements++) {
-        printf("%d", array[printed_elements]);
+    for (int printed_elements = 0; printed_elements < number_of_elements; printed_elements++) {
+        printf("%i", array[printed_elements]);
     }
     printf("\n");
 }
@@ -25,90 +25,115 @@ void bubble_sort(int array[], const int number_of_elements)
 }
 
 
-void merge(int array[], int l_idx, int m_idx, int r_idx)
-{
-    int left_array_lenght = m_idx - l_idx + 1;
-    int right_array_lenght = r_idx - m_idx;
-
-    int* left_array;
-    int* right_array;
-
-    left_array = malloc(left_array_lenght * sizeof(int));
-    right_array = malloc(right_array_lenght * sizeof(int));
-
-    if (left_array == NULL || right_array == NULL) {
-        printf("Memory allocation error\n");
-        exit(1);
-    }
-
+void merge(int array[], int l_idx, int m_idx, int r_idx, int temp[])           
+{                                                                               
+                                                                            
     int l_arr_counter, r_arr_counter, full_arr_counter;
 
-    for (l_arr_counter = 0; l_arr_counter < left_array_lenght; l_arr_counter++) {
-        left_array[l_arr_counter] = array[l_idx + l_arr_counter];
+    for (l_arr_counter = l_idx; l_arr_counter < m_idx + 1; l_arr_counter++) {
+       temp[l_arr_counter] = array[l_arr_counter];
     }
 
-    for (r_arr_counter = 0; r_arr_counter < right_array_lenght; r_arr_counter++) {
-        right_array[r_arr_counter] = array[m_idx + 1 + r_arr_counter];
+    for (r_arr_counter = m_idx + 1; r_arr_counter < r_idx + 1; r_arr_counter++) {
+        temp[r_arr_counter] = array[r_arr_counter];
     }
 
-    l_arr_counter = 0;
-    r_arr_counter = 0;
+    l_arr_counter = l_idx;
+    r_arr_counter = m_idx + 1;
     full_arr_counter = l_idx;
 
-    while (r_arr_counter < right_array_lenght && l_arr_counter < left_array_lenght) {
-        if (right_array[r_arr_counter] < left_array[l_arr_counter]) {
-            array[full_arr_counter] = right_array[r_arr_counter];
+    while (r_arr_counter < r_idx + 1 && l_arr_counter < m_idx + 1) {
+        if (temp[r_arr_counter] < temp[l_arr_counter]) {
+            array[full_arr_counter] = temp[r_arr_counter];
             ++r_arr_counter;
         } else {
-            array[full_arr_counter] = left_array[l_arr_counter];
+            array[full_arr_counter] = temp[l_arr_counter];
             ++l_arr_counter;
         }
         ++full_arr_counter;
     }
 
-    while (r_arr_counter < right_array_lenght) {
-        array[full_arr_counter] = right_array[r_arr_counter];
+    while (r_arr_counter < r_idx + 1) {
+        array[full_arr_counter] = temp[r_arr_counter];
         ++r_arr_counter;
         ++full_arr_counter;
     }
 
-    while (l_arr_counter < left_array_lenght) {
-        array[full_arr_counter] = left_array[l_arr_counter];
+    while (l_arr_counter < m_idx + 1) {
+        array[full_arr_counter] = temp[l_arr_counter];
         ++l_arr_counter;
         ++full_arr_counter;
     }
 
-    left_array = NULL;
-    right_array = NULL;
 }
 
 
-void merge_sort_recursion(int array[], int l_idx, int r_idx)
+void merge_sort_recursion(int array[], int l_idx, int r_idx, int temp[])
 {
     if (l_idx < r_idx) {
 
     int m_idx = l_idx + (r_idx - l_idx) / 2;
 
-    merge_sort_recursion(array, l_idx, m_idx);
-    merge_sort_recursion(array, m_idx + 1, r_idx);
+    merge_sort_recursion(array, l_idx, m_idx, temp);
+    merge_sort_recursion(array, m_idx + 1, r_idx, temp);
 
-    merge(array, l_idx, m_idx, r_idx);
+    merge(array, l_idx, m_idx, r_idx, temp);
     }
 }
 
 void merge_sort(int array[], int lenght)
 {
-    merge_sort_recursion(array, 0, lenght - 1);
+    int* temp;
+    temp = malloc(lenght * sizeof(int));
+    if (temp == NULL) {
+        printf("Allocation error\n");
+        exit(1);
+    }
+
+    merge_sort_recursion(array, 0, lenght - 1, temp);
+
+    temp = NULL;
+}
+
+
+void insertion_sort(int array[], const int lenght)
+{
+    int out_elements;
+    int in_elements;
+    int current_element;
+
+    for(out_elements = 1; out_elements < lenght; out_elements++)
+    {
+        current_element = array[out_elements];
+
+        in_elements = out_elements - 1;
+        while(in_elements >= 0)
+        {
+            if(current_element < array[in_elements])
+            {
+                array[in_elements + 1] = array[in_elements]; // 9 2 3 4    9 9 3 4
+            }
+            else
+            {
+                break;
+            }
+            in_elements = in_elements - 1;
+        }
+
+        array[in_elements + 1] = current_element;
+    }
+
+
 }
 
 
 int main()
 {
-    int mass[] = {9, 2, 6, 3, 7, 5};
+    int mass[] = {9, 2, 6, 3, 5, 7};
 
     int size = sizeof(mass) / sizeof(int);
 
-    merge_sort(mass, size);
+    insertion_sort(mass, size);
 
     mass_print(mass, size);
 }
