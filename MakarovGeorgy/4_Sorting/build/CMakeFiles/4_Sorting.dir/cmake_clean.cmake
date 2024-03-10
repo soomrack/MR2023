e02a2764f0e0d0c144b3e0039dc81f7e335ab0e8
Aristotel <124895720+simeonidi03@users.noreply.github.com>
2024-03-10 23:35:49 +0300

    alpha

---
+++ MakarovGeorgy/4_Sorting/build/CMakeFiles/4_Sorting.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "4_Sorting.exe"
+  "4_Sorting.exe.manifest"
+  "4_Sorting.pdb"
+  "CMakeFiles/4_Sorting.dir/main.cpp.obj"
+  "CMakeFiles/4_Sorting.dir/main.cpp.obj.d"
+  "lib4_Sorting.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/4_Sorting.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
