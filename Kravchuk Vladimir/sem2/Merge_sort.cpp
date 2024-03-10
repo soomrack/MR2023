7416825374a70855b0d3c9d09d33822674365728
vladimirkrv <144235541+vladimirkrv@users.noreply.github.com>
2024-03-10 23:00:23 +0300

    Merge_sort.cpp

--- Kravchuk Vladimir/sem2/Merge_sort.cpp
+++ Kravchuk Vladimir/sem2/Merge_sort.cpp
@@ -2,45 +2,42 @@
 #include <vector>
 #include <cstring>
 
-void merge(std::vector<int>& array, int initial, int middle, int last,  int* buff)
+void merge(int* array, int initial, int middle, int last)
 {
-	int countInFirst = middle + 1 - initial;
-	int countInSecond = last - middle;
+	int i = initial;
+	int j = middle + 1;
 
-	memcpy(buff, &array[initial], countInFirst * sizeof(int));
-
-	int i = 0; // The element of the buff array
-	int j = middle + 1; // The element of the unsorted array
-	int k = initial; // The element of the sorted array
-
-	while (i < countInFirst && j <= last) {
-		if (buff[i] <= array[j]) {
-			array[k] = buff[i];
+	while (i <= middle && j <= last) {
+		if (array[i] <= array[j]) {
 			i++;
-		} else 
-		{
-			array[k] = array[j];
-			j++;
 		}
-		k++;
-	}
+		else {
+			int buff = array[j];
+			for (int k = j; k > i; k--) {
+				array[k] = array[k - 1];
+			}
+			array[i] = buff;
 
-	memcpy(&array[k], &buff[i], (countInFirst - i) * sizeof(int));
+			j++;
+			middle++;
+		}
+	}
 }
 
-void sort(std::vector<int>& array, int initial, int last, int* buff) 
+void sort(int* array, int initial, int last)
 {
-	if (initial < last) {
+	if (initial < last) 
+	{
 		int middle = initial + (last - initial) / 2;
 
-		sort(array, initial, middle, buff);
-		sort(array, middle + 1, last, buff);
+		sort(array, initial, middle);
+		sort(array, middle + 1, last);
 
-		merge(array, initial, middle, last,  buff);
+		merge(array, initial, middle, last);
 	}
 }
 
-void printArray(int size, std::vector<int>& array)
+void printArray(int size, int* array)
 {
 	std::cout << "\nSorted array: ";
 	for (int idx = 0; idx < size; idx++) {
@@ -50,16 +47,12 @@ void printArray(int size, std::vector<int>& array)
 
 void initialization()
 {
-	std::vector<int> array = { 1, 11, 7, 5, 9, 10, 14 };
-	int size = array.size();
+	int array[] = { 1, 11, 7, 5, 9, 10, 14, 15, 3 };
+	int size = std::end(array) - std::begin(array);
 
-	int* buff = new int[size];
-
-	sort(array, 0, size - 1, buff);
+	sort(array, 0, size - 1);
 
 	printArray(size, array);
-
-	delete[] buff;
 }
 
 int main() 
