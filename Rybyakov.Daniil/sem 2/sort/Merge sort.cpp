3d515783fbd7a590d5c9dc45d4bfa00777b0c48d
rybyakov19 <144235855+rybyakov19@users.noreply.github.com>
2024-03-11 18:54:13 +0300

    Update Merge sort.cpp

--- Rybyakov.Daniil/sem 2/sort/Merge sort.cpp
+++ Rybyakov.Daniil/sem 2/sort/Merge sort.cpp
@@ -2,14 +2,14 @@
 using namespace std;
 
 
-void merge_sort(int* help_array, int* array, int array_size)
+void merge(int* help_array, int* array, int array_size)
 {
     int middle = array_size / 2;
 
     if (array_size < 2) return;
 
-    merge_sort(help_array, array, middle);
-    merge_sort(help_array + middle, array + middle, array_size - middle);
+    merge(array, help_array, middle);
+    merge(array + middle, help_array + middle, array_size - middle);
 
     for (int idx = 0, left = 0, right = middle; idx < array_size; idx++) {
         if (right >= array_size) help_array[idx] = array[left++];
@@ -17,12 +17,15 @@ void merge_sort(int* help_array, int* array, int array_size)
         else if (array[left] <= array[right]) help_array[idx] = array[left++];
         else help_array[idx] = array[right++];
     }
-
-    for (int i = 0; i < array_size; i++) {
-        array[i] = help_array[i];
-    }
 }
 
+void merge_sort(int* array, int array_size)
+{
+    int* help_array = new int[array_size];
+    memcpy(help_array, array, sizeof(int) * array_size);
+    merge(help_array, array, array_size);
+    delete[] help_array;
+}
 
 void array_print(int array_size, int* array)
 {
@@ -37,16 +40,12 @@ void initialization()
 {
     int array_size = 10;
     int* array = new int[10] { 2, 4, 1, 11, 9, 7, 4, 9, 50, 20 };
-    int help_array_size = 10;
-    int* help_array = new int[10];
 
     array_print(array_size, array);
-  
-    merge_sort(help_array, array, array_size);
+    merge_sort(array, array_size);
     array_print(array_size, array);
 
     delete[] array;
-    delete[] help_array;
 }
 
 
