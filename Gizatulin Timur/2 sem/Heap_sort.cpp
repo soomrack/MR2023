a0c5ef91f479f44bc87867adb86102b62a8f501c
TimurGit11314 <144234955+TimurGit11314@users.noreply.github.com>
2024-03-11 14:15:19 +0300

    123

---
+++ Gizatulin Timur/2 sem/Heap_sort.cpp
@@ -0,0 +1,67 @@
+﻿#include <iostream>
+
+using namespace std;
+
+void Swap(int* A, int* B)
+{
+    int tmp = *A;
+    *A = *B;
+    *B = tmp;
+}
+
+
+void heapify(int* arr, int size, int idx) {
+    int largest = idx;
+    int left = 2 * idx + 1;
+    int right = 2 * idx + 2;
+
+    if (left < size && arr[left] > arr[largest]) {
+        largest = left;
+    }
+
+    if (right < size && arr[right] > arr[largest]) {
+        largest = right;
+    }
+
+    if (largest != idx) {
+        Swap(&arr[idx], &arr[largest]);
+        heapify(arr, size, idx);
+    }
+}
+
+void heap_sort(int* arr, int size)
+{
+    // binary tree
+    for (int idx = size / 2 - 1; idx >= 0; idx--)
+    {
+        heapify(arr, size, idx);
+    }
+    // Извлекаем элементы из дерева в порядке убывания по индексу
+    for (int idx = size - 1; idx > 0; idx--)
+    {
+        Swap(&arr[0], &arr[idx]);
+
+        heapify(arr, idx, 0);
+    }
+}
+
+void print_arr(int* m, int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        cout << m[i] << " ";
+    }
+
+    cout << endl;
+}
+
+int main()
+{
+    int size = 5;
+    int* arr = new int[5] { 8, 4, 1, 11, 20 };
+    print_arr(arr, size);
+    heap_sort(arr, size);
+    delete[] arr;
+    return 0;
+}
+
