7dcb3c16091da4cf21bfab6a39e5ad74f277fb28
Aristotel <124895720+simeonidi03@users.noreply.github.com>
2024-04-05 23:51:45 +0300

    A1

---
+++ Papasymeonidis Aristotelis/Task5/build/CMakeFiles/Task5.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Task5.dir/main.cpp.o"
+  "CMakeFiles/Task5.dir/main.cpp.o.d"
+  "Task5"
+  "Task5.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Task5.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
