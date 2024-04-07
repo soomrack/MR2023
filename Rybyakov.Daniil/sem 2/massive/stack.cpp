1e1824616c1378be50e2af0de367f100db4ed40e
rybyakov19 <144235855+rybyakov19@users.noreply.github.com>
2024-04-07 14:57:18 +0300

    Update stack.cpp

--- Rybyakov.Daniil/sem 2/massive/stack.cpp
+++ Rybyakov.Daniil/sem 2/massive/stack.cpp
@@ -1,72 +1,60 @@
 #include <iostream>
 
-
-template<typename T>
 class Node {
 private:
-    template<typename> friend class Stack;
+    friend class Stack;
+    int val;
     Node* next;
 public:
-    Node(T _val) : val(_val), next(nullptr) {}
-    T val;
+    Node(int _val) : val(_val), next(nullptr) {}
 };
 
-
-template<typename T>
 class Stack {
 private:
-    Node<T>* st;
-    Node<T>* last;
+    Node* st;
+    Node* last;
 public:
     Stack() : st(nullptr), last(nullptr) {}
     bool is_empty();
-    void push(T _val);
+    void push(int _val);
     void print();
     void pop();
 };
 
-
-template<typename T>
-bool Stack<T>::is_empty() {
+bool Stack::is_empty() {
     return st == nullptr;
 }
 
-
-template<typename T>
-void Stack<T>::push(T _val) {
-    Node<T>* el = new Node<T>(_val);
+void Stack::push(int _val) {
+    Node* el = new Node(_val);
     if (is_empty()) {
         st = el;
         last = el;
-        return;
     }
-    el->next = st;
-    st = el;
+    else {
+        el->next = st;
+        st = el;
+    }
 }
 
-
-template<typename T>
-void Stack<T>::print() {
+void Stack::print() {
     if (is_empty()) {
         std::cout << "stack is empty" << std::endl;
-        return;
     }
-    std::cout << st->val << std::endl;
+    else {
+        std::cout << st->val << std::endl;
+    }
 }
 
-
-template<typename T>
-void Stack<T>::pop() {
+void Stack::pop() {
     if (is_empty()) return;
-    Node<T>* el = st;
+    Node* el = st;
     st = el->next;
     delete el;
 }
 
-
-
 int main() {
-    Stack<int> A;
+    Stack A;
     A.push(3);
     std::cout << "is empty? " << A.is_empty() << std::endl;
     A.print();
