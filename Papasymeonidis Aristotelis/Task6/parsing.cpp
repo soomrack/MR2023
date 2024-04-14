20ccc34254013435c0538bc16ffcd7cd5c78c16c
Aristotel <124895720+simeonidi03@users.noreply.github.com>
2024-04-14 14:06:59 +0300

    TheBestFlightIsLED-SKG

---
+++ Papasymeonidis Aristotelis/Task6/parsing.cpp
@@ -0,0 +1,66 @@
+/*
+start project: 14/04/2024
+tg/inst/git/vk - simeonidi03
+*/
+
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <sstream>
+
+using namespace std;
+
+void print_flights(vector<Flight>& Flights);
+void Write_to_file(ofstream& logfile, vector<Flight>& Flights);
+
+class Flight
+{
+public:
+    Flight(double FLIGHT_TIME, double AIRLINE_ID, std::string UNIQUE_AIRLINES_NAME, double ORIGIN_IATA_AIRPORT_CODE,
+           std::string ORIGIN_CITY, double DEST_IATA_AIRPORT_CODE, std::string DEST_CITY);
+
+    void print();
+
+    double time;
+    double airline_id;
+    std::string airline_name;
+    double city_from_id;
+    std::string city_from;
+    double city_to_id;
+    std::string city_to;
+};
+
+
+Flight::Flight(double FLIGHT_TIME, double AIRLINE_ID, std::string UNIQUE_AIRLINES_NAME, double ORIGIN_IATA_AIRPORT_CODE, std::string ORIGIN_CITY, double DEST_IATA_AIRPORT_CODE, std::string DEST_CITY){
+    time = FLIGHT_TIME;
+    airline_id = AIRLINE_ID;
+    airline_name = UNIQUE_AIRLINES_NAME;
+    city_from_id = ORIGIN_IATA_AIRPORT_CODE;
+    city_from = ORIGIN_CITY;
+    city_to_id = DEST_IATA_AIRPORT_CODE;
+    city_to = DEST_CITY;
+}
+
+
+void Flight::print(){
+
+   std::cout << "Flight time: " << time << std::endl;
+   std::cout << "Airline ID: " << airline_id << std::endl;
+   std::cout << "Airline name: " << airline_name << std::endl;
+   std::cout << "Origin IATA Airport code: " << city_from_id << std::endl;
+   std::cout << "Origin city: " << city_from << std::endl;
+   std::cout << "Destination IATA Airport code: " << city_to_id << std::endl;
+   std::cout << "Dest city: " << city_to << std::endl;
+   std::cout << std::endl;
+
+}
+
+
+void print_flights(vector<Flight>& Flights){
+
+    for (auto flight : Flights) {
+        flight.print();
+    }
+}
