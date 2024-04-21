d56ea10a370838929c6a62d2f3876b921fdbc091
Aristotel <124895720+simeonidi03@users.noreply.github.com>
2024-04-22 01:27:56 +0300

    star_alliance

--- Papasymeonidis_Aristotelis/Task6/parsing.cpp
+++ Papasymeonidis_Aristotelis/Task6/parsing.cpp
@@ -61,7 +61,53 @@ void print_flights(std::vector<Flight>& Flights){
     }
 }
 
+
+void write_to_file(std::ofstream& new_file, std::vector<Flight>& Flights)
+{
+    new_file << "fr.id" << "\t" 
+        << "to.id" << "\t" 
+        << "time" << "\t" 
+        << "car.id" << "\t" 
+        << "carrier.name" << "\t" 
+        << "ci.fr." << "\t" 
+        << "ci.to" << std::endl;
+    for (auto flight : Flights) {
+        new_file << flight.city_from_id << "\t" 
+            << flight.city_to_id << "\t" 
+            << flight.time << "\t" 
+            << flight.airline_id << "\t" 
+            << flight.airline_name << "\t" 
+            << flight.city_from << "\t" 
+            << flight.city_to << std::endl;
+    }
+}
+
+void source_file_is_open(std::ifstream& datafile){
+    if(datafile.is_open()){
+        std::cerr<<"File is open"<<std::endl;
+    }else{
+        std::cerr<<"File is not open"<<std::endl;
+    }
+}
+
 int main(){
+    std::ifstream datafile("/home/simeonidi03/Documents/GitHub/MR2023/Papasymeonidis_Aristotelis/Task6/dataterm.csv");
+    std::ofstream logfile("new_data.txt");
+    source_file_is_open(datafile);
+
+    std::vector<Flight> Flights;
+    std::string line;
+
+    while(not datafile.eof()){
+
+    }
+
+    uint i = 0;
+
+
+
+    datafile.close();
+    logfile.close();
 
     return 0;
 }
