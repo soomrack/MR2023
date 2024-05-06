31a800de39a9dfb01125897098aa1441febd24ac
syrupsweety <44460187+syrupsweety@users.noreply.github.com>
2024-05-06 15:17:46 +0300

    Update queue.cpp

--- Buraev Nikita/semester2/queue.cpp
+++ Buraev Nikita/semester2/queue.cpp
@@ -1,74 +1,106 @@
 #include <iostream>
 #include <stdexcept>
 
-class Node {
-private:
-    friend class Queue;
-    size_t val;
-    Node* next;
-
-public:
-    Node(int _val) : val(_val), next(nullptr) {}
-};
-
 class Queue {
 private:
-    Node* head;
-    Node* tail;
+    class Node {
+    public:
+        int data;
+        Node* next;
+
+        Node(int value) : data(value), next(nullptr) {}
+    };
+
+    Node* front;
+    Node* end;
+    size_t size;
 
 public:
-    Queue() : head(nullptr), tail(nullptr) {}
+    Queue();
+    ~Queue();
 
-    bool isEmpty() const;
-    void push(int _val);
-    void print() const;
-    int pop();
+    void enqueue(int);
+    int dequeue();
+    int front_element();
+    size_t get_size();
+    bool is_empty();
+    void clear();
 };
 
-class QueueException : public std::domain_error {
-public:
-    QueueException(const char* const message) : std::domain_error(message) {}
-};
+Queue::Queue() : front(nullptr), end(nullptr), size(0) {}
 
-const QueueException emptyQueue("Queue is empty");
-
-void Queue::push(int _val) {
-    Node* el = new Node(_val);
-    if (head == nullptr) {
-        head = tail = el;
-        return;
-    }
-    tail->next = el;
-    tail = el;
+Queue::~Queue() {
+    clear();
 }
 
-void Queue::print() const {
-    if (head == nullptr) return;
-    Node* el = head;
-    while (el) {
-        std::cout << el->val << " ";
-        el = el->next;
+void Queue::enqueue(int value) {
+    Node* newNode = new Node(value);
+    if (end == nullptr) {
+        front = end = newNode;
+    } else {
+        end->next = newNode;
+        end = newNode;
     }
-    std::cout << std::endl;
+    size++;
 }
 
-int Queue::pop() {
-    if (head == nullptr) throw emptyQueue;
-    Node* el = head;
-    int headVal = el->val;
-    head = el->next;
-    delete el;
-    return headVal;
+int Queue::dequeue() {
+    if (is_empty()) {
+        throw std::runtime_error("Error: Queue is empty!");
+    }
+
+    Node* temp = front;
+    int dequeuedValue = temp->data;
+    front = front->next;
+    if (front == nullptr) {
+        end = nullptr;
+    }
+    delete temp;
+    size--;
+
+    return dequeuedValue;
+}
+
+int Queue::front_element() {
+    if (is_empty()) {
+        throw std::runtime_error("Error: Queue is empty!");
+    }
+    return front->data;
+}
+
+size_t Queue::get_size() {
+    return size;
+}
+
+bool Queue::is_empty() {
+    return size == 0;
+}
+
+void Queue::clear() {
+    Node* current = front;
+    while (current != nullptr) {
+        Node* temp = current;
+        current = current->next;
+        delete temp;
+    }
+    front = end = nullptr;
+    size = 0;
 }
 
 int main() {
-    Queue A;
-    A.push(1);
-    std::cout << "Is empty? " << A.isEmpty() << std::endl;
-    A.push(2);
-    A.print();
-    A.push(3);
-    A.pop();
-    A.print();
+    Queue queue;
+    queue.enqueue(5);
+    queue.enqueue(10);
+    queue.enqueue(15);
+
+    std::cout << "Queue size: " << queue.get_size() << std::endl;
+    std::cout << "Front element: " << queue.front_element() << std::endl;
+
+    std::cout << "Dequeueing elements: ";
+    while (!queue.is_empty()) {
+        std::cout << queue.dequeue() << " ";
+    }
+    std::cout << std::endl;
+
     return 0;
 }
