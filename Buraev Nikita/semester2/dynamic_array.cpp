e62b02cef0e7a6255a7cf4f7b9bf2e686277eaf8
syrupsweety <44460187+syrupsweety@users.noreply.github.com>
2024-05-06 15:10:37 +0300

    Update dynamic_array.cpp

--- Buraev Nikita/semester2/dynamic_array.cpp
+++ Buraev Nikita/semester2/dynamic_array.cpp
@@ -1,77 +1,142 @@
 #include <iostream>
+#include <cstring>
+#include <initializer_list>
+
+// DynamicArray implementation
 
 class DynamicArray {
-private:
-    int* buffer; //memory space dynamically allocated
-    int size; //number of elements currently in use
-    int allocatedSize; //size of the memory space allocated (buffer)
-
 public:
     DynamicArray();
-    explicit DynamicArray(int initialAllocatedSize);
+    DynamicArray(const int[], const size_t);
+    DynamicArray(const std::initializer_list<int>&, const size_t);
+    DynamicArray(const std::initializer_list<int>&, const size_t, const size_t);
+    DynamicArray(const DynamicArray&);
+
     ~DynamicArray();
 
-    int getElem(int index) const;
-    void setElem(int index, int value);
-    void add(int value);
+    void push_back(int);
+    void pop_back();
+    void insert(int, size_t);
+    void erase();
+    void set_buffer(size_t);
+    void print_array();
+    void resize(int);
 
-    void resize(int newSize);
+    int operator[](size_t);
+
+private:
+    int* data;
+    size_t size;
+    size_t buffer;
+    size_t free;
 };
 
 DynamicArray::DynamicArray() {
-    allocatedSize = 1;
     size = 0;
-    buffer = new int[allocatedSize];
+    free = 5;   // default array size
+    buffer = 5; // default buffer
+    data = new int[free];
 }
 
-DynamicArray::DynamicArray(int initialAllocatedSize) {
-    allocatedSize = initialAllocatedSize;
-    size = 0;
-    buffer = new int[allocatedSize];
+DynamicArray::DynamicArray(const int array[], const size_t size)
+    : size(size) {
+    buffer = 5; // default buffer
+    data = new int[size + buffer];
+    free = buffer;
+    std::memcpy(data, array, sizeof(int) * size);
+}
+
+DynamicArray::DynamicArray(const std::initializer_list<int>& array, const size_t size)
+    : size(size) {
+    buffer = 5; // default buffer
+    data = new int[size + buffer];
+    std::copy(array.begin(), array.end(), data);
+}
+
+DynamicArray::DynamicArray(const std::initializer_list<int>& array, const size_t size, const size_t buffer)
+    : size(size), free(buffer), buffer(buffer) {
+    data = new int[size + buffer];
+    std::copy(array.begin(), array.end(), data);
+}
+
+DynamicArray::DynamicArray(const DynamicArray& other) {
+    size = other.size;
+    free = other.free;
+    data = new int[size + free];
+    std::memcpy(data, other.data, sizeof(int) * size);
 }
 
 DynamicArray::~DynamicArray() {
-    delete[] buffer;
+    delete[] data;
 }
 
-int DynamicArray::getElem(int index) const {
-    return buffer[index];
+void DynamicArray::set_buffer(size_t buffer) {
+    this->buffer = buffer;
 }
 
-void DynamicArray::setElem(int index, int value) {
-    buffer[index] = value;
-}
-
-void DynamicArray::add(int value) {
-    if (size == allocatedSize) {
-        resize(allocatedSize + 5);
+void DynamicArray::resize(int new_size) {
+    if (new_size > size + free) {
+        int* new_data = new int[new_size + buffer];
+        std::memcpy(new_data, data, sizeof(int) * size);
+        delete[] data;
+        size = new_size;
+        data = new_data;
+        free = buffer;
     }
-    buffer[size++] = value;
-}
-
-void DynamicArray::resize(int newSize) {
-    int* newBuffer = new int[newSize];
-    for (int i = 0; i < size; i++) {
-        newBuffer[i] = buffer[i];
+    else {
+        free = size + free - new_size;
+        size = new_size;
     }
-    delete[] buffer;
-    buffer = newBuffer;
-    allocatedSize = newSize;
 }
 
-int main() {
-    DynamicArray arr(3);
-    arr.setElem(2, 5);
-    std::cout << arr.getElem(2) << std::endl;
+void DynamicArray::push_back(int element) {
+    resize(size + 1);
+    data[size--] = element;
+}
 
-    arr.add(22);
-    arr.add(3);
-    arr.add(128);
+void DynamicArray::pop_back() {
+    if (size == 0) {
+        throw std::runtime_error("Error: DynamicArray is empty!");
+    }
+    data[size - 1] = 0;
+    size--;
+    free++;
+}
 
-    arr.resize(5);
+void DynamicArray::insert(int element, size_t idx) {
+    if (idx < 0 || idx >= size) {
+        throw std::out_of_range("Error: Invalid index!");
+    }
 
-    arr.add(33);
-    arr.add(28);
+    if (free == 0) {
+        resize(size + 1);
+    }
 
-    return 0;
+    std::memcpy(&data[idx + 1], &data[idx], sizeof(int) * (size - idx));
+    data[idx] = element;
+    size++;
+    free--;
+}
+
+void DynamicArray::erase() {
+    std::memset(data, 0, sizeof(int) * (size));
+    free += size;
+    size = 0;
+}
+
+int DynamicArray::operator[](size_t idx) {
+    return data[idx];
+}
+
+void DynamicArray::print_array() {
+    if (size == 0) {
+        std::cout << "[]" << std::endl;
+        return;
+    }
+
+    std::cout << "[";
+    for (size_t idx = 0; idx < size; idx++) {
+        std::cout << data[idx];
+        (idx < size - 1) ? std::cout << ", " : std::cout << "]" << std::endl;
+    }
 }
