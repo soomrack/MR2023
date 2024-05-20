4bd9a83ccec43f2c0ecc2ea6d25f380b7a21b992
paralarnax <semkarim2018@gmail.com>
2024-05-20 13:10:43 +0300

    20.05

---
+++ Каримуллин Семён/AbstractStructures/dynamic_array.cpp
@@ -0,0 +1,83 @@
+#include <iostream>
+
+class DynamicArray {
+private:
+    int* data;
+    size_t size;
+    size_t size_allocated;
+
+public:
+    size_t bufer;
+    DynamicArray();
+    ~DynamicArray();
+    explicit DynamicArray(size_t size , size_t buffer);
+    int get_elem(size_t index) const;
+    void set_elem(size_t index, int value);
+    void add(int value);
+    void resize(size_t new_size);
+};
+
+DynamicArray::DynamicArray(): size{0}, bufer{1}, data {nullptr} {
+}
+
+DynamicArray::DynamicArray(size_t size, size_t bufer): size{size}, bufer{bufer}, size_allocated {size + bufer} {
+    if (size_allocated == 0){
+        data = nullptr;
+    }
+    else {
+    data = new int[size_allocated];
+    }
+}
+
+DynamicArray::~DynamicArray() {
+    delete[] data;
+}
+
+int DynamicArray::get_elem(size_t index) const {
+  if (index >= size) {
+        return data[index];
+    }
+    return 0;
+}
+    
+void DynamicArray::set_elem(size_t index, int value){
+    if (size <  index) return;
+    data[index] = value;
+}
+
+void DynamicArray::add(int value){
+    resize(size++);
+    data[size--] = value;
+}
+
+void DynamicArray::resize(size_t new_size) {
+    if (new_size > size_allocated)
+     {
+        size_allocated = new_size + bufer;
+        int* new_data = new int[size_allocated];
+        for (size_t i = 0; i < size; i++){
+            new_data[i] = data[i];
+        }
+        delete[] data;
+        data = new_data;
+    }
+    size = new_size;
+}
+
+int main()
+{
+    DynamicArray arr(0,3);
+    arr.set_elem(2, 5);
+    std::cout << arr.get_elem(2) << std::endl;
+
+    arr.add(22);
+    arr.add(3);
+    arr.add(128);
+
+    arr.resize(5);
+
+    arr.add(33);
+    arr.add(28);
+
+    return 0;
+}
---
+++ Каримуллин Семён/AbstractStructures/linked_list.cpp
@@ -0,0 +1,127 @@
+#include <iostream>
+
+struct Node
+{
+    int data;
+    Node* next;
+};
+
+class List
+{
+private:
+    Node* head;
+    Node* tail;
+
+public:
+    List() : head(nullptr), tail(nullptr) {}
+    ~List() {clear_list();}
+    void push_tail(int value);
+    void display();
+    void push_head(int value);
+    void delete_head();
+    void delete_tail();
+    void delete_position(int pos);
+    void clear_list();
+};
+
+void List::push_tail(int value) 
+{
+    Node* temp = new Node;
+    temp->data = value;
+    temp->next = nullptr;
+
+    if (head == nullptr)
+    {
+        head = temp;
+        tail = temp;
+        temp = nullptr;
+    }
+    else
+    {
+        tail->next = temp;
+        tail = temp;
+    }
+}
+
+
+void List::display() { 
+    Node* temp = new Node;
+    temp = head;
+    while (temp != nullptr)
+    {
+        std::cout << temp->data << "\t";
+        temp = temp->next;
+    }
+}
+
+
+void List::push_head(int value) {
+    Node* temp = new Node;
+    temp->data = value;
+    temp->next = head;
+    head = temp;
+}
+
+
+void List::delete_head() {
+    Node* temp = new Node;
+    temp = head;
+    head = head->next;
+    delete temp;
+}
+
+
+void List::delete_tail()
+{
+    Node* current = new Node;
+    Node* previous = new Node;
+    current = head;
+    while (current->next != nullptr)
+    {
+        previous = current;
+        current = current->next;
+    }
+    tail = previous;
+    previous->next = NULL;
+    delete current;
+}
+
+
+void List::delete_position(int pos) {
+    Node* current = new Node;
+    Node* previous = new Node;
+    current = head;
+    for (int i = 1; i < pos; i++)
+    {
+        previous = current;
+        current = current->next;
+    }
+    previous->next = current->next;
+}
+
+void List::clear_list()
+{
+    head = nullptr;
+    tail = nullptr;
+}
+
+
+int main()
+{
+    List list;
+    list.display();
+
+    list.push_tail(55);
+
+    list.push_head(50);
+
+    list.delete_head();
+
+    list.delete_tail();
+
+    list.delete_position(4);
+    
+    list.clear_list();
+
+    return 0;
+}
