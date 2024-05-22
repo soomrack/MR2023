1844fb874acbe11859c9cc9a0a8fa81de23c91bf
syrupsweety <44460187+syrupsweety@users.noreply.github.com>
2024-05-22 05:55:23 +0300

    Update dijkstra.cpp

--- Buraev Nikita/semester2/dijkstra.cpp
+++ Buraev Nikita/semester2/dijkstra.cpp
@@ -2,49 +2,58 @@
 #include <fstream>
 #include <sstream>
 #include <vector>
-#include <limits>
+#include <limits.h>
 #include <algorithm>
-#include <queue>
 
 using namespace std;
 
 constexpr int OFFSET = 30000;
-constexpr int INF = numeric_limits<int>::max();
+constexpr int INF = INT_MAX;
 
 class Vertex {
 public:
-    int previous_vertex = 0;
+    int previous_vertex = -1;
     int distance_from_start = INF;
 };
 
-class Edge {
+class Neighbor {
 public:
-    int destination;
-    int weight;
+    int vertex;
+    int distance;
 };
 
-void dijkstra(int start, int end, const vector<vector<Edge>>& graph)
-{
-    int num_vertices = graph.size();
-    vector<Vertex> vertices(num_vertices);
+class CompareDistance {
+public:
+    bool operator()(const Neighbor& n1, const Neighbor& n2) {
+        return n1.distance > n2.distance;
+    }
+};
+
+void dijkstra(int start, int end, const vector<vector<Neighbor>>& adjacency_list) {
+    int n = adjacency_list.size();
+    vector<Vertex> vertices(n);
+    vector<Neighbor> heap;
+    heap.push_back({start, 0});
     vertices[start].distance_from_start = 0;
-    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
-    pq.push({0, start});
+    push_heap(heap.begin(), heap.end(), CompareDistance());
 
-    while (!pq.empty()) {
-        int current_distance = pq.top().first;
-        int current_vertex = pq.top().second;
-        pq.pop();
+    while (!heap.empty()) {
+        int current_vertex = heap.front().vertex;
+        int current_distance = heap.front().distance;
+        pop_heap(heap.begin(), heap.end(), CompareDistance());
+        heap.pop_back();
 
-        if (current_distance > vertices[current_vertex].distance_from_start)
+        if (current_distance > vertices[current_vertex].distance_from_start) {
             continue;
+        }
 
-        for (const Edge& edge : graph[current_vertex]) {
-            int new_distance = current_distance + edge.weight;
-            if (new_distance < vertices[edge.destination].distance_from_start) {
-                vertices[edge.destination].distance_from_start = new_distance;
-                vertices[edge.destination].previous_vertex = current_vertex;
-                pq.push({new_distance, edge.destination});
+        for (const Neighbor& neighbor : adjacency_list[current_vertex]) {
+            int new_distance = current_distance + neighbor.distance;
+            if (new_distance < vertices[neighbor.vertex].distance_from_start) {
+                vertices[neighbor.vertex].distance_from_start = new_distance;
+                vertices[neighbor.vertex].previous_vertex = current_vertex;
+                heap.push_back({neighbor.vertex, new_distance});
+                push_heap(heap.begin(), heap.end(), CompareDistance());
             }
         }
     }
@@ -55,26 +64,24 @@ void dijkstra(int start, int end, const vector<vector<Edge>>& graph)
         cout << "Shortest path: ";
         vector<int> path;
         for (int idx_vertex = end; idx_vertex != start; idx_vertex = vertices[idx_vertex].previous_vertex) {
-            path.push_back(idx_vertex + OFFSET);
+            path.push_back(OFFSET + idx_vertex);
         }
-        path.push_back(start + OFFSET);
+        path.push_back(OFFSET + start);
         reverse(path.begin(), path.end());
         for (int vertex : path) {
-            cout << vertex << " <- ";
+            cout << vertex << " ";
         }
         cout << endl;
         cout << "Time: " << vertices[end].distance_from_start << endl;
     }
 }
 
-vector<vector<Edge>> parse_graph(const string& filename, int matrix_size)
-{
-    vector<vector<Edge>> graph(matrix_size);
+void fill_adjacency_list(vector<vector<Neighbor>>& adjacency_list, int matrix_size) {
+    ifstream datafile("parsed.csv");
 
-    ifstream datafile(filename);
     if (!datafile.is_open()) {
-        cerr << "Error: Couldn't open parsed file" << endl;
-        return graph;
+        cout << "Error: Couldn't open parsed file" << endl;
+        return;
     }
 
     string line;
@@ -82,43 +89,47 @@ vector<vector<Edge>> parse_graph(const string& filename, int matrix_size)
         stringstream inputStream(line);
         int source, destination, time;
         char comma;
+
         inputStream >> source >> comma >> destination >> comma >> time;
+
         source -= OFFSET;
         destination -= OFFSET;
+
         if (source < matrix_size && destination < matrix_size) {
-            graph[source].push_back({destination, time});
+            adjacency_list[source].push_back({destination, time});
         }
     }
-
-    return graph;
 }
 
-int main()
-{
-    const string filename = "parsed.csv";
-    const int matrix_size = 10000;
-    vector<vector<Edge>> graph = parse_graph(filename, matrix_size);
+void start() {
+    int matrix_size = 10000;
+    int start = 0, end = 0;
+    unsigned int state, run = 1;
 
-    int start, end;
-    char state;
-    do {
-        cout << "Press 'c' to continue, 'q' to exit: ";
+    vector<vector<Neighbor>> adjacency_list(matrix_size);
+
+    fill_adjacency_list(adjacency_list, matrix_size);
+
+    while (run) {
+        cout << "Press 1 to continue, 0 to exit: ";
         cin >> state;
-        switch (state) {
-            case 'c':
-                cout << "start: ";   cin >> start;
-                cout << "end: ";     cin >> end;
-                start -= OFFSET;
-                end -= OFFSET;
-                dijkstra(start, end, graph);
-                break;
-            case 'q':
-                cout << "Exiting..." << endl;
-                break;
-            default:
-                cout << "Invalid option" << endl;
-        }
-    } while (state != 'q');
 
+        switch (state) {
+        case 0:
+            run = 0;
+            break;
+        case 1:
+            cout << "start: ";   cin >> start;
+            cout << "end: ";     cin >> end;
+            start -= OFFSET;
+            end -= OFFSET;
+            dijkstra(start, end, adjacency_list);
+            break;
+        }
+    }
+}
+
+int main() {
+    start();
     return 0;
 }
