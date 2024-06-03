3d23ad19b18a62a35fb189a65ede849b09b94b64
Marsel <marslanov481@gmail.com>
2024-06-03 11:00:53 +0300

    deleted build

---
+++ Arslanov.Marsel/ray_casting/build/CMakeFiles/ray_casting.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/marsel/Polikek/programming/ray_casting/main.cpp" "CMakeFiles/ray_casting.dir/main.cpp.o" "gcc" "CMakeFiles/ray_casting.dir/main.cpp.o.d"
+  "/home/marsel/Polikek/programming/ray_casting/source/scene.cpp" "CMakeFiles/ray_casting.dir/source/scene.cpp.o" "gcc" "CMakeFiles/ray_casting.dir/source/scene.cpp.o.d"
+  "/home/marsel/Polikek/programming/ray_casting/source/shapes.cpp" "CMakeFiles/ray_casting.dir/source/shapes.cpp.o" "gcc" "CMakeFiles/ray_casting.dir/source/shapes.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Arslanov.Marsel/ray_casting/build/CMakeFiles/ray_casting.dir/source/shapes.cpp.o.d
@@ -0,0 +1,3 @@
+CMakeFiles/ray_casting.dir/source/shapes.cpp.o: \
+ /home/marsel/Polikek/programming/ray_casting/source/shapes.cpp \
+ /usr/include/stdc-predef.h
