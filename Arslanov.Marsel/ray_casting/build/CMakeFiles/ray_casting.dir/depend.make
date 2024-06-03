54c8b36471f3296864c0228f00d90e56ab2dc8bd
Marsel <marslanov481@gmail.com>
2024-06-03 10:52:52 +0300

    remove build  from repository

--- Arslanov.Marsel/ray_casting/build/CMakeFiles/ray_casting.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for ray_casting.
-# This may be replaced when dependencies are built.
--- Arslanov.Marsel/ray_casting/build/CMakeFiles/ray_casting.dir/source/shapes.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/ray_casting.dir/source/shapes.cpp.o: \
- /home/marsel/Polikek/programming/ray_casting/source/shapes.cpp \
- /usr/include/stdc-predef.h
