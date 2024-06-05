839951ac31978feb957d3cbb50e83d0d147e51e6
TimurGit11314 <144234955+TimurGit11314@users.noreply.github.com>
2024-06-05 21:53:28 +0300

    upd

--- Gizatullin Timur/2 sem/airports/dijkstra.cpp
+++ Gizatullin Timur/2 sem/airports/dijkstra.cpp
@@ -33,15 +33,18 @@ public:
 void dijkstra(int start, int end, const vector<vector<int>>& adjacency_matrix) {
     int matrix_size = adjacency_matrix.size();
     vector<Vertex> vertices(matrix_size);
-    priority_queue<Neighbor, vector<Neighbor>, CompareDistance> min_heap;
+    priority_queue<Neighbor, vector<Neighbor>, CompareDistance> min_queue;
 
     vertices[start].distance_from_start = 0;
-    min_heap.push({start, 0});
+    min_queue.push({start, 0});
 
-    while (!min_heap.empty())
+    while (!min_queue.empty())
     {
-        auto [current_vertex, current_distance] = min_heap.top();
-        min_heap.pop();
+        Neighbor top = min_queue.top();
+        int current_vertex = top.vertex;
+        int current_distance = top.distance;
+
+        min_queue.pop();
 
         if (current_distance > vertices[current_vertex].distance_from_start) {
             continue;
@@ -56,7 +59,7 @@ void dijkstra(int start, int end, const vector<vector<int>>& adjacency_matrix) {
                 {
                     vertices[neighbor].distance_from_start = new_distance;
                     vertices[neighbor].previous_vertex = current_vertex;
-                    min_heap.push({neighbor, new_distance});
+                    min_queue.push({neighbor, new_distance});
                 }
             }
         }
@@ -102,30 +105,30 @@ void fill_adjacency_matrix(vector<vector<int>>& adjacency_matrix, const string& 
     }
 }
 
-void start() {
+void run() {
     const int matrix_size = 10000;
     vector<vector<int>> adjacency_matrix(matrix_size, vector<int>(matrix_size, INF));
 
     fill_adjacency_matrix(adjacency_matrix, "parsed.csv");
 
     while (true) {
-        int start, end;
+        int start_id, end_id;
         cout << "start:";
-        cin >> start;
+        cin >> start_id;
         cout << "end:";
-        cin >> end;
+        cin >> end_id;
 
-        start -= OFFSET;
-        end -= OFFSET;
+        start_id -= OFFSET;
+        end_id -= OFFSET;
 
-        if (start >= 0 && start < matrix_size && end >= 0 && end < matrix_size) {
-            dijkstra(start, end, adjacency_matrix);} 
+        if (start_id >= 0 && start_id < matrix_size && end_id >= 0 && end_id < matrix_size) {
+            dijkstra(start_id, end_id, adjacency_matrix);} 
         else{
             cout << "Invalid vertices" << endl;}
     }
 }
 
 int main() {
-    start();
+    run();
     return 0;
 }
