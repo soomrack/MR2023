afa4acb3f6cb5af2a7bf507d98311599b7b4d788
Networksx333 <127897894+Networksx333@users.noreply.github.com>
2024-09-19 21:04:49 +0300

    Наверное, это нужно

--- Sudneko Vladislav/RobotProject/Raspberry/build/CMakeFiles/OmegabotManualControlRS.dir/DependInfo.cmake
+++ Sudneko Vladislav/RobotProject/Raspberry/build/CMakeFiles/OmegabotManualControlRS.dir/DependInfo.cmake
@@ -1,30 +1,22 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
 # The set of languages for which implicit dependencies are needed:
 set(CMAKE_DEPENDS_LANGUAGES
-  "CXX"
-  )
-# The set of files for implicit dependencies of each language:
-set(CMAKE_DEPENDS_CHECK_CXX
-  "/home/pi/Rasberry/src/main.cpp" "/home/pi/Rasberry/build/CMakeFiles/OmegabotManualControlRS.dir/src/main.cpp.o"
-  )
-set(CMAKE_CXX_COMPILER_ID "GNU")
-
-# Preprocessor definitions for this target.
-set(CMAKE_TARGET_DEFINITIONS_CXX
-  "BOOST_ALL_NO_LIB"
-  "BOOST_SYSTEM_DYN_LINK"
   )
 
-# The include file search paths:
-set(CMAKE_CXX_TARGET_INCLUDE_PATH
-  "."
-  "../"
-  "/usr/include/gstreamer-1.0"
-  "/usr/include/glib-2.0"
-  "/usr/lib/arm-linux-gnueabihf/glib-2.0/include"
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/vladislav/Documents/GitHub/MR2023/Sudneko Vladislav/RobotProject/Raspberry/src/main.cpp" "CMakeFiles/OmegabotManualControlRS.dir/src/main.cpp.o" "gcc" "CMakeFiles/OmegabotManualControlRS.dir/src/main.cpp.o.d"
   )
 
-# Targets to which this target links.
-set(CMAKE_TARGET_LINKED_INFO_FILES
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
   )
 
 # Fortran module output directory.
--- Sudneko Vladislav/RobotProject/Raspberry/build/CMakeFiles/OmegabotManualControlRS.dir/cmake_clean.cmake
+++ Sudneko Vladislav/RobotProject/Raspberry/build/CMakeFiles/OmegabotManualControlRS.dir/cmake_clean.cmake
@@ -1,5 +1,6 @@
 file(REMOVE_RECURSE
   "CMakeFiles/OmegabotManualControlRS.dir/src/main.cpp.o"
+  "CMakeFiles/OmegabotManualControlRS.dir/src/main.cpp.o.d"
   "OmegabotManualControlRS"
   "OmegabotManualControlRS.pdb"
 )
