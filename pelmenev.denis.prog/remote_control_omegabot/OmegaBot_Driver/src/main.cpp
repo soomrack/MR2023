0e9ebfd5fb01c5fac3e1b9e5f4ae606d76c07552
denispelm <144234708+denispelm@users.noreply.github.com>
2024-12-24 19:27:28 +0300

    Omegabot Final?

--- pelmenev.denis.prog/remote_control_omegabot/OmegaBot_Driver/src/main.cpp
+++ pelmenev.denis.prog/remote_control_omegabot/OmegaBot_Driver/src/main.cpp
@@ -1,20 +1,24 @@
 #include "driver.hpp"
 
-
-Driver Wheels(6, 5, 7, 4, 10, 9);  // Инициализация с пинами для моторов
-
+Driver Wheels(6, 5, 7, 4, A1, A2, 200);  // Инициализация с пинами для моторов
 
 void setup() {
     Serial.begin(9600);
-    delay(100);
+    delay(3000);
+    Serial.println("Arduino started. Logging initialized...");
 }
 
-
 void loop() {
-    if (Serial.available() > 0) {
-        Wheels.get_command_arm();
-        Wheels.get_command_wheels();
-    }
+    char command = Wheels.read_command();
+    if (command != '0') { 
+        unsigned long currentMillis = millis();
 
-    delay(40);
+        Wheels.get_command_arm(command);
+        Wheels.get_command_wheels(command);
+        Wheels.get_command_other(command);
+
+        Wheels.write_logs(command, currentMillis);
+
+        Wheels.clear_serial_buffer();
+    }
 }
